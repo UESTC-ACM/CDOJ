d7142177fd5f5e2afcdb10db24e35d5efee5dbfc
muziriyun@gmail.com <muziriyun@gmail.com@ee32a38b-bb68-4f76-8b42-4a8d2bfd8f77>
2012-12-22 13:58:52 +0000

    自己写了个cpp，可以把多个html拼接起来方便统一修改
    git-svn-id: https://cdoj.googlecode.com/svn@24 ee32a38b-bb68-4f76-8b42-4a8d2bfd8f77

---
+++ trunk/static/src/main.cpp
@@ -0,0 +1,81 @@
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+char buf[1<<16];
+
+string Error(string message)
+{
+	cout << "Error in Gao : " << message << endl;
+	return "<span class=\"label label-important\">"+message+"</span>";
+}
+
+string Gao(string file)
+{
+	if (file == "")
+		return Error("Empty filename !");
+
+	string filename = file+".html";
+	FILE *pfile = fopen(filename.c_str(),"r");
+	if (pfile == NULL)
+		return Error("Can not open "+filename+"!");
+
+	string content = "";
+	while (fgets(buf,1<<16,pfile) != NULL)
+		content = content+buf;
+	fclose(pfile);
+
+	/* phrase */
+	/* #include(xxx) */
+
+	string sflag = "#include(";
+	string eflag = ")";
+	string target;
+	int spos,epos;
+	while (true)
+	{
+		spos = content.find(sflag);
+		if (spos == content.npos)
+			break;
+
+		epos = content.find(eflag,spos);
+		if (epos == content.npos)
+			break;
+
+		target = content.substr(spos+sflag.size(),epos-spos-sflag.size()-eflag.size()+1);
+
+		content.replace(spos,epos-spos+1,Gao(target));
+	}
+
+	return content;
+}
+
+string Gen(string file)
+{
+	if (file == "")
+		return "Empty filename !";
+
+	string filename = "../"+file+".html";
+	FILE *pfile = fopen(filename.c_str(),"w");
+	if (pfile == NULL)
+		return "Can not open "+filename+"!";
+
+	fprintf(pfile,"%s",Gao(file).c_str());
+
+	fclose(pfile);
+
+	return "Success build "+filename+"!";
+}
+
+int main()
+{
+	cout << Gen("index") << endl;
+	cout << Gen("problem") << endl;
+	cout << Gen("problem_single") << endl;
+	cout << Gen("problem_single_submit") << endl;
+
+	return 0;
+}
