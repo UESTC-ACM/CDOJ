db96803f38fe62907fabf090d9fe0e91cac0cbc2
lyhypacm@gmail.com <lyhypacm@gmail.com@ee32a38b-bb68-4f76-8b42-4a8d2bfd8f77>
2012-12-27 15:22:59 +0000

    struts and hibernate framework added.
    git-svn-id: https://cdoj.googlecode.com/svn@29 ee32a38b-bb68-4f76-8b42-4a8d2bfd8f77

---
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/action/admin/AdminWelcome.java
@@ -0,0 +1,15 @@
+package cn.edu.uestc.acmicpc.action.admin;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: fish
+ * Date: 12-12-26
+ * Time: 下午3:03
+ */
+public class AdminWelcome extends ActionSupport {
+    public String toWelcome() throws Exception {
+        return SUCCESS;
+    }
+}
