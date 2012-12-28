5aac47dc106d33b88473a3b663c7e67a08282505
lyhypacm@gmail.com <lyhypacm@gmail.com@ee32a38b-bb68-4f76-8b42-4a8d2bfd8f77>
2012-12-28 03:57:18 +0000

    copyright profile added.
    git-svn-id: https://cdoj.googlecode.com/svn@30 ee32a38b-bb68-4f76-8b42-4a8d2bfd8f77

--- branches/include/AppException.h
+++ branches/include/AppException.h
@@ -1,10 +1,20 @@
-/**
- * Copyright 2012, fish <lyhypacm@gmail.com>
+/*
+ * cdoj, UESTC ACMICPC Online Judge
+ * Copyright (c) 2012  fish <@link lyhypacm@gmail.com>
  *
- * Application Exception Class
- *		all customized exception of this project
- * will use this class to deal with customized
- * actions.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 #ifndef __APPEXCEPTION_H__
 #define __APPEXCEPTION_H__
@@ -13,6 +23,13 @@
 
 // buffer's size limit of application exception
 #define	APPBUFSIZ	1024
+
+/**
+ * Application Exception Class
+ *		all customized exception of this project
+ * will use this class to deal with customized
+ * actions.
+ */
 
 class AppException {
 public:
