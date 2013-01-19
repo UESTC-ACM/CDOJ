be295d44de05a92aa8eda8f4a439ee82c0c17752
lyhypacm@gmail.com <lyhypacm@gmail.com@ee32a38b-bb68-4f76-8b42-4a8d2bfd8f77>
2013-01-19 09:45:24 +0000

    DataBase Entities added. Entity and DAO designed.
    git-svn-id: https://cdoj.googlecode.com/svn@64 ee32a38b-bb68-4f76-8b42-4a8d2bfd8f77

---
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/db/dao/DAO.java
@@ -0,0 +1,83 @@
+/*
+ *
+ *  * cdoj, UESTC ACMICPC Online Judge
+ *  * Copyright (c) 2013 fish <@link lyhypacm@gmail.com>,
+ *  * 	mzry1992 <@link muziriyun@gmail.com>
+ *  *
+ *  * This program is free software; you can redistribute it and/or
+ *  * modify it under the terms of the GNU General Public License
+ *  * as published by the Free Software Foundation; either version 2
+ *  * of the License, or (at your option) any later version.
+ *  *
+ *  * This program is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  * GNU General Public License for more details.
+ *  *
+ *  * You should have received a copy of the GNU General Public License
+ *  * along with this program; if not, write to the Free Software
+ *  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+package cn.edu.uestc.acmicpc.db.dao;
+
+import cn.edu.uestc.acmicpc.db.dao.base.BaseDAO;
+import cn.edu.uestc.acmicpc.db.dao.base.IDAO;
+import cn.edu.uestc.acmicpc.util.AppException;
+import org.hibernate.HibernateException;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Global DAO implementation.
+ *
+ * @param <Entity> Entity's type
+ * @param <PK>     Primary key's type
+ * @author <a href="mailto:lyhypacm@gmail.com">fish</a>
+ * @version 1
+ */
+public abstract class DAO<Entity extends Serializable, PK extends Serializable>
+        extends BaseDAO implements IDAO<Entity, PK> {
+    @Override
+    public Serializable add(Entity entity) throws AppException {
+        return getSession().save(entity);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public Entity get(PK key) throws AppException {
+        return (Entity) getSession().get(getReferenceClass(), key);
+    }
+
+    @Override
+    public void update(Entity entity) throws AppException {
+        getSession().update(entity);
+    }
+
+    @Override
+    public void delete(Entity entity) throws AppException {
+        getSession().delete(entity);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<Entity> findAll() throws AppException {
+        List<Entity> list;
+        try {
+            list = getSession().createCriteria(getReferenceClass()).list();
+        } catch (HibernateException e) {
+            throw new AppException("Invoke findAll method error.");
+        }
+        return list;
+    }
+
+    /**
+     * Return the specific Object class that will be used for class-specific
+     * implementation of this DAO.
+     *
+     * @return the reference Class
+     */
+    protected abstract Class<Entity> getReferenceClass();
+}
---
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/db/dao/base/IDAO.java
@@ -0,0 +1,80 @@
+/*
+ *
+ *  * cdoj, UESTC ACMICPC Online Judge
+ *  * Copyright (c) 2013 fish <@link lyhypacm@gmail.com>,
+ *  * 	mzry1992 <@link muziriyun@gmail.com>
+ *  *
+ *  * This program is free software; you can redistribute it and/or
+ *  * modify it under the terms of the GNU General Public License
+ *  * as published by the Free Software Foundation; either version 2
+ *  * of the License, or (at your option) any later version.
+ *  *
+ *  * This program is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  * GNU General Public License for more details.
+ *  *
+ *  * You should have received a copy of the GNU General Public License
+ *  * along with this program; if not, write to the Free Software
+ *  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+package cn.edu.uestc.acmicpc.db.dao.base;
+
+import cn.edu.uestc.acmicpc.util.AppException;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Global DAO interface.
+ *
+ * @param <Entity> Entity's type
+ * @param <PK>     Primary key's type
+ * @author <a href="mailto:lyhypacm@gmail.com">fish</a>
+ * @version 1
+ */
+public interface IDAO<Entity extends Serializable, PK extends Serializable> {
+    /**
+     * Add entity into database, and return number of Row changed.
+     *
+     * @param entity entity to be added.
+     * @return number of rows changed.
+     * @throws AppException
+     */
+    public Serializable add(Entity entity) throws AppException;
+
+    /**
+     * Get entity by key value.
+     *
+     * @param key key value
+     * @return entity which key value matches
+     * @throws AppException
+     */
+    public Entity get(PK key) throws AppException;
+
+    /**
+     * Update an entity object.
+     *
+     * @param entity entity to be updated
+     * @throws AppException
+     */
+    public void update(Entity entity) throws AppException;
+
+    /**
+     * Delete entity from database.
+     *
+     * @param entity entity to be deleted
+     * @throws AppException
+     */
+    public void delete(Entity entity) throws AppException;
+
+    /**
+     * List all entities in tables.
+     *
+     * @return entity list in tables.
+     * @throws AppException
+     */
+    public List<Entity> findAll() throws AppException;
+}
