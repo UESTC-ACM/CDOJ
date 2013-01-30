b92d57c1c6c93329b70187537378aebb42227375
lyhypacm@gmail.com <lyhypacm@gmail.com@ee32a38b-bb68-4f76-8b42-4a8d2bfd8f77>
2013-01-30 10:58:20 +0000

    [FIXED] new view can not work well problem view updated.
    git-svn-id: https://cdoj.googlecode.com/svn@124 ee32a38b-bb68-4f76-8b42-4a8d2bfd8f77

---
+++ trunk/src/test/java/cn/edu/uestc/acmicpc/oj/test/db/ViewTest.java
@@ -0,0 +1,73 @@
+/*
+ *
+ *  * cdoj, UESTC ACMICPC Online Judge
+ *  * Copyright (c) 2013 fish <@link lyhypacm@gmail.com>,
+ *  * 	mzry1992 <@link muziriyun@gmail.com>
+ *  *
+ *  * This program is free software; you can redistribute it and/or
+ *  * modify it under the terms of the GNU General Public License
+ *  * as published by the Free Software Foundation; either version 2
+ *  * of the License, or (at your option) any later version.
+ *  *
+ *  * This program is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  * GNU General Public License for more details.
+ *  *
+ *  * You should have received a copy of the GNU General Public License
+ *  * along with this program; if not, write to the Free Software
+ *  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+package cn.edu.uestc.acmicpc.oj.test.db;
+
+import cn.edu.uestc.acmicpc.oj.db.dao.iface.IDepartmentDAO;
+import cn.edu.uestc.acmicpc.oj.db.entity.User;
+import cn.edu.uestc.acmicpc.oj.db.view.impl.UserView;
+import cn.edu.uestc.acmicpc.oj.ioc.DepartmentDAOAware;
+import cn.edu.uestc.acmicpc.util.StringUtil;
+import cn.edu.uestc.acmicpc.util.exception.AppException;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+/**
+ * Test cases for view.
+ *
+ * @author <a href="mailto:lyhypacm@gmail.com">fish</a>
+ * @version 1
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContext.xml", "classpath:applicationContext-test.xml"})
+public class ViewTest implements DepartmentDAOAware {
+    @Autowired
+    private IDepartmentDAO departmentDAO;
+
+    @Test
+    public void testUserView() throws AppException {
+        User user = new User();
+        user.setUserName("admin");
+        user.setPassword(StringUtil.encodeSHA1("admin"));
+        user.setNickName("admin");
+        user.setEmail("acm@uestc.edu.cn");
+        user.setSchool("UESTC");
+        user.setDepartmentByDepartmentId(departmentDAO.get(1));
+        user.setStudentId("2010013100008");
+        user.setLastLogin(new Timestamp(new Date().getTime()));
+        UserView userView = new UserView(user);
+        Assert.assertEquals("admin", userView.getUserName());
+    }
+
+    @Override
+    public void setDepartmentDAO(IDepartmentDAO departmentDAO) {
+        this.departmentDAO = departmentDAO;
+    }
+}
