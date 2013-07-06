bac24d74e263ec77e284089927476ba1c3c93b4c
mzry1992 <muziriyun@gmail.com>
2013-07-06 19:58:51 +0800

    [FIX] @Version on Training entity

--- trunk/src/main/java/cn/edu/uestc/acmicpc/db/entity/TrainingStatus.java
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/db/entity/TrainingStatus.java
@@ -71,8 +71,8 @@ public class TrainingStatus implements Serializable {
 
     private Integer version;
 
+    @Version
     @Column(name = "OPTLOCK")
-    @Basic
     public Integer getVersion() {
         return version;
     }
