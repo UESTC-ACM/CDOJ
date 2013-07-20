ecc6b3f5452a3762f1a7a01e80bec1fc6bec599f
RuinsHe <lyhypacm@gmail.com>
2013-07-20 22:05:23 +0800

    [REVIEWED] code reviewd: ProbleamCondition.java.

--- trunk/src/main/java/cn/edu/uestc/acmicpc/db/condition/base/BaseCondition.java
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/db/condition/base/BaseCondition.java
@@ -1,21 +1,23 @@
 /*
- * cdoj, UESTC ACMICPC Online Judge
- * Copyright (c) 2012  fish <@link lyhypacm@gmail.com>,
- * 	mzry1992 <@link muziriyun@gmail.com>
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ *  cdoj, UESTC ACMICPC Online Judge
+ *  Copyright (c) 2013 fish <@link lyhypacm@gmail.com>,
+ *  	mzry1992 <@link muziriyun@gmail.com>
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
  */
 
 package cn.edu.uestc.acmicpc.db.condition.base;
@@ -231,21 +233,22 @@ public abstract class BaseCondition implements ApplicationContextAware {
           String fieldName = StringUtil.getFieldNameFromGetterOrSetter(method.getName());
           Object value, keyValue;
           Exp exp = method.getAnnotation(Exp.class);
-          if (exp == null)
+          if (exp == null) {
             continue;
+          }
           try {
             value = method.invoke(this);
           } catch (Exception e) {
             continue;
           }
-          if (value == null)
+          if (value == null) {
             continue;
+          }
           keyValue = value;
           String mapField = exp.MapField();
           mapField = StringUtil.isNullOrWhiteSpace(mapField) ? fieldName : mapField;
-          mapField =
-              upperCaseFirst ? mapField.substring(0, 1).toUpperCase() + mapField.substring(1)
-                  : mapField;
+          mapField = upperCaseFirst ?
+              mapField.substring(0, 1).toUpperCase() + mapField.substring(1) : mapField;
           boolean isJoinedProperty = false;
           if (!exp.MapObject().equals(objectClass)) {
             String name =
@@ -258,10 +261,9 @@ public abstract class BaseCondition implements ApplicationContextAware {
           if (exp.Type().name().equals("like")) {
             value = String.format("%%%s%%", value);
           }
-          Criterion c =
-              (Criterion) restrictionsClass
-                  .getMethod(exp.Type().name(), String.class, Object.class).invoke(null, mapField,
-                      value);
+          Criterion c = (Criterion) restrictionsClass
+              .getMethod(exp.Type().name(), String.class, Object.class)
+                  .invoke(null, mapField, value);
           if (isJoinedProperty) {
             condition.addJoinedProperty(mapField, new JoinedProperty(c, keyValue, exp.Type()));
           } else {
--- trunk/src/main/java/cn/edu/uestc/acmicpc/db/condition/base/Condition.java
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/db/condition/base/Condition.java
@@ -1,22 +1,22 @@
 /*
  *
- *  * cdoj, UESTC ACMICPC Online Judge
- *  * Copyright (c) 2013 fish <@link lyhypacm@gmail.com>,
- *  * 	mzry1992 <@link muziriyun@gmail.com>
- *  *
- *  * This program is free software; you can redistribute it and/or
- *  * modify it under the terms of the GNU General Public License
- *  * as published by the Free Software Foundation; either version 2
- *  * of the License, or (at your option) any later version.
- *  *
- *  * This program is distributed in the hope that it will be useful,
- *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  * GNU General Public License for more details.
- *  *
- *  * You should have received a copy of the GNU General Public License
- *  * along with this program; if not, write to the Free Software
- *  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *  cdoj, UESTC ACMICPC Online Judge
+ *  Copyright (c) 2013 fish <@link lyhypacm@gmail.com>,
+ *  	mzry1992 <@link muziriyun@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
  */
 
@@ -58,8 +58,9 @@ public class Condition {
   private Map<String, JoinedProperty> joinedProperties;
 
   public Map<String, JoinedProperty> getJoinedProperties() {
-    if (joinedProperties == null)
+    if (joinedProperties == null) {
       joinedProperties = new HashMap<>();
+    }
     return joinedProperties;
   }
 
@@ -93,8 +94,9 @@ public class Condition {
   }
 
   public List<Criterion> getCriterionList() {
-    if (criterionList == null)
+    if (criterionList == null) {
       criterionList = new LinkedList<>();
+    }
     return criterionList;
   }
 
@@ -103,8 +105,9 @@ public class Condition {
   }
 
   public List<Order> getOrders() {
-    if (orders == null)
+    if (orders == null) {
       orders = new LinkedList<>();
+    }
     return orders;
   }
 
@@ -113,8 +116,9 @@ public class Condition {
   }
 
   public List<Projection> getProjections() {
-    if (projections == null)
+    if (projections == null) {
       projections = new LinkedList<>();
+    }
     return projections;
   }
 
@@ -139,9 +143,9 @@ public class Condition {
   public Condition(Long currentPage, Long countPerPage, String field, Boolean asc) {
     this.currentPage = currentPage;
     this.countPerPage = countPerPage;
-    if (field != null && asc != null)
+    if (field != null && asc != null) {
       addOrder(field, asc);
-
+    }
   }
 
   /**
