23766311bf697ba39892d17e4ae18a244d633fea
RuinsHe <lyhypacm@gmail.com>
2013-07-21 11:53:37 +0800

    [REVIEWED] code reviewd: Article.java.

--- trunk/src/main/java/cn/edu/uestc/acmicpc/db/dao/iface/ITrainingUserDAO.java
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/db/dao/iface/ITrainingUserDAO.java
@@ -1,22 +1,23 @@
 /*
- * cdoj, UESTC ACMICPC Online Judge
  *
- * Copyright (c) 2013 fish <@link lyhypacm@gmail.com>,
- * mzry1992 <@link muziriyun@gmail.com>
+ *  cdoj, UESTC ACMICPC Online Judge
+ *  Copyright (c) 2013 fish <@link lyhypacm@gmail.com>,
+ *  	mzry1992 <@link muziriyun@gmail.com>
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
  */
 
 package cn.edu.uestc.acmicpc.db.dao.iface;
@@ -24,7 +25,7 @@ package cn.edu.uestc.acmicpc.db.dao.iface;
 import cn.edu.uestc.acmicpc.db.entity.TrainingUser;
 
 /**
- * Description
+ * TrainingUserDAO AOP interface.
  * 
  * @author <a href="mailto:muziriyun@gmail.com">mzry1992</a>
  */
--- trunk/src/main/java/cn/edu/uestc/acmicpc/db/dto/impl/StatusDTO.java
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/db/dto/impl/StatusDTO.java
@@ -1,3 +1,25 @@
+/*
+ *
+ *  cdoj, UESTC ACMICPC Online Judge
+ *  Copyright (c) 2013 fish <@link lyhypacm@gmail.com>,
+ *  	mzry1992 <@link muziriyun@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
 package cn.edu.uestc.acmicpc.db.dto.impl;
 
 import cn.edu.uestc.acmicpc.db.dto.base.BaseDTO;
@@ -10,6 +32,7 @@ import java.sql.Timestamp;
 import java.util.Date;
 
 /**
+ * // TODO(fish) Description
  * @author <a href="mailto:lyhypacm@gmail.com">fish</a>
  */
 public class StatusDTO extends BaseDTO<Status> {
