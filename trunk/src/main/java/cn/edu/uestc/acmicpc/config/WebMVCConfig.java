d1b46714f32fa9322391852c787a728a7b8a9f6d
mzry1992 <muziriyun@gmail.com>
2013-09-18 14:51:47 +0800

    [UPDATE] Use WebMVCConfig.java instead of mvc-dispatcher-servlet.xml

---
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/config/WebMVCConfig.java
@@ -0,0 +1,63 @@
+package cn.edu.uestc.acmicpc.config;
+
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+import java.util.List;
+
+/**
+ * Description
+ *
+ * @author <a href="mailto:muziriyun@gmail.com">mzry1992</a>
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {
+  "cn.edu.uestc.acmicpc.oj.controller"
+})
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+public class WebMVCConfig extends WebMvcConfigurationSupport {
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    System.out.println("FFFFFFFFFFFFFFFF");
+    registry.addResourceHandler("/images/**").addResourceLocations("/images/**");
+    registry.addResourceHandler("/plugins/**").addResourceLocations("/plugins/**");
+    registry.addResourceHandler("/scripts/**").addResourceLocations("/scripts/**");
+    registry.addResourceHandler("/styles/**").addResourceLocations("/styles/**");
+  }
+
+  @Override
+  public void configureDefaultServletHandling(DefaultServletHandlerConfigurer defaultServletHandlerConfigurer) {
+    defaultServletHandlerConfigurer.enable();
+  }
+
+  @Override
+  public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+    converters.add(new FastJsonHttpMessageConverter());
+
+    addDefaultHttpMessageConverters(converters);
+  }
+
+  @Bean
+  public ViewResolver viewResolver() {
+    System.out.println("Fuck !");
+    InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+
+    viewResolver.setPrefix("/WEB-INF/views/");
+    viewResolver.setSuffix(".jsp");
+
+    return viewResolver;
+  }
+
+}
