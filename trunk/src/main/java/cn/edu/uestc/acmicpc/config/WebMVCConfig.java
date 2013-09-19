70db28efce127564c54e6aa3dcda6df552b2825d
mzry1992 <muziriyun@gmail.com>
2013-09-19 11:48:40 +0800

    [FIX] forget import package

--- trunk/src/main/java/cn/edu/uestc/acmicpc/config/WebMVCConfig.java
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/config/WebMVCConfig.java
@@ -8,10 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
