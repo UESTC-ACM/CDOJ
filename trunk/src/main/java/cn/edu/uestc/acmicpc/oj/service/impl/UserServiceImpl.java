2bb65c8cf40abfc553478f2fcb4c1d2bf18cf22b
mzry1992 <muziriyun@gmail.com>
2013-09-20 14:16:07 +0800

    [UPDATE] User regist serveice added

--- trunk/src/main/java/cn/edu/uestc/acmicpc/oj/service/impl/UserServiceImpl.java
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/oj/service/impl/UserServiceImpl.java
@@ -3,6 +3,10 @@ package cn.edu.uestc.acmicpc.oj.service.impl;
 import java.sql.Timestamp;
 import java.util.Date;
 
+import cn.edu.uestc.acmicpc.db.dao.iface.IDepartmentDAO;
+import cn.edu.uestc.acmicpc.ioc.dao.DepartmentDAOAware;
+import cn.edu.uestc.acmicpc.util.Global;
+import cn.edu.uestc.acmicpc.util.exception.FieldException;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,16 +22,20 @@ import cn.edu.uestc.acmicpc.oj.service.iface.UserService;
 import cn.edu.uestc.acmicpc.util.StringUtil;
 import cn.edu.uestc.acmicpc.util.exception.AppException;
 import cn.edu.uestc.acmicpc.util.exception.FieldNotUniqueException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 
 /**
  * Implementation for {@link UserService}.
  */
 @Service
-public class UserServiceImpl extends AbstractService implements UserService, UserDAOAware {
+public class UserServiceImpl extends AbstractService implements UserService, UserDAOAware, DepartmentDAOAware {
 
   private static final Logger LOGGER = LogManager.getLogger(UserServiceImpl.class);
   @Autowired
   private IUserDAO userDAO;
+  @Autowired
+  private IDepartmentDAO departmentDAO;
 
   @Override
   public void setUserDAO(IUserDAO userDAO) {
@@ -68,7 +76,7 @@ public class UserServiceImpl extends AbstractService implements UserService, Use
   public UserDTO login(UserLoginDTO userLoginDTO) throws AppException {
     User user = getUserByUserName(userLoginDTO.getUserName());
     if (user == null || !StringUtil.encodeSHA1(userLoginDTO.getPassword()).equals(user.getPassword()))
-      return null;
+      throw new FieldException("password", "User or password is wrong, please try again");
 
     user.setLastLogin(new Timestamp(new Date().getTime() / 1000 * 1000));
     userDAO.update(user);
@@ -84,6 +92,34 @@ public class UserServiceImpl extends AbstractService implements UserService, Use
   }
 
   @Override
+  public UserDTO register(UserDTO userDTO) throws AppException {
+    /*
+    @FieldExpressionValidator(fieldName = "userDTO.departmentId",
+              expression = "userDTO.departmentId in global.departmentList.{departmentId}",
+              key = "error.department.validation") },
+     */
+    if (getUserByUserName(userDTO.getUserName()) != null)
+      throw new FieldException("userName", "User name has been used!");
+    if (getUserByEmail(userDTO.getEmail()) != null)
+      throw new FieldException("email", "Email has benn used!");
+
+    User user = new User();
+    user.setTried(0);
+    user.setNickName(userDTO.getNickName());
+    user.setDepartmentByDepartmentId(departmentDAO.get(userDTO.getDepartmentId()));
+    user.setEmail(userDTO.getEmail());
+    user.setLastLogin(new Timestamp(new Date().getTime() / 1000 * 1000));
+    user.setPassword(StringUtil.encodeSHA1(userDTO.getPassword()));
+    user.setSchool(userDTO.getSchool());
+    user.setSolved(0);
+    user.setStudentId(userDTO.getStudentId());
+    user.setType(Global.AuthenticationType.NORMAL.ordinal());
+    user.setUserName(userDTO.getUserName());
+    createNewUser(user);
+    return userDTO;
+  }
+
+  @Override
   public User getUserByUserId(Integer userId) throws AppException {
     return userDAO.get(userId);
   }
@@ -91,5 +127,10 @@ public class UserServiceImpl extends AbstractService implements UserService, Use
   @Override
   public IDAO<User, Integer> getDAO() {
     return userDAO;
+  }
+
+  @Override
+  public void setDepartmentDAO(IDepartmentDAO departmentDAO) {
+    this.departmentDAO = departmentDAO;
   }
 }
