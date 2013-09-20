63ed7f4285f7a8a13f34c1c70863e42dba789061
mzry1992 <muziriyun@gmail.com>
2013-09-20 16:44:19 +0800

    [UPDATE] Add department list in register model, add trim function

--- trunk/src/main/java/cn/edu/uestc/acmicpc/service/iface/GlobalService.java
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/service/iface/GlobalService.java
@@ -3,6 +3,8 @@ package cn.edu.uestc.acmicpc.service.iface;
 import cn.edu.uestc.acmicpc.db.entity.Department;
 import cn.edu.uestc.acmicpc.util.Global;
 
+import java.util.List;
+
 /**
  * Description
  *
@@ -13,4 +15,6 @@ public interface GlobalService {
   public Global getGlobal();
 
   public Department getDepartmentByDepartmentId(Integer departmentId);
+
+  public List<Department> getDepartmentList();
 }
