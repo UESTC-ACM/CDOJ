8a29297a8df2e36893b5432426ebd46eb48e2e64
RuinsHe <lyhypacm@gmail.com>
2013-09-21 19:25:21 +0800

    [TEST] fix mock dao issue.

--- trunk/src/test/java/cn/edu/uestc/acmicpc/oj/service/UserServiceTest.java
+++ trunk/src/test/java/cn/edu/uestc/acmicpc/oj/service/UserServiceTest.java
@@ -14,11 +14,13 @@ import org.springframework.test.context.web.WebAppConfiguration;
 
 import cn.edu.uestc.acmicpc.config.MockDAOContext;
 import cn.edu.uestc.acmicpc.db.dao.iface.IUserDAO;
+import cn.edu.uestc.acmicpc.db.dto.impl.UserDTO;
 import cn.edu.uestc.acmicpc.db.dto.impl.UserLoginDTO;
 import cn.edu.uestc.acmicpc.db.entity.User;
 import cn.edu.uestc.acmicpc.oj.service.iface.UserService;
 import cn.edu.uestc.acmicpc.util.StringUtil;
 import cn.edu.uestc.acmicpc.util.exception.AppException;
+import cn.edu.uestc.acmicpc.util.exception.FieldException;
 import cn.edu.uestc.acmicpc.util.exception.FieldNotUniqueException;
 
 /** Test cases for {@link UserService}. */
@@ -42,5 +44,46 @@ public class UserServiceTest {
     when(userDAO.getEntityByUniqueField("userName", userLoginDTO.getUserName()))
         .thenReturn(user);
     Assert.assertNotNull(userService.login(userLoginDTO));
+  }
+
+  @Test
+  public void testLogin_failed_notSuchUser() throws AppException, FieldNotUniqueException {
+    try {
+      UserLoginDTO userLoginDTO = UserLoginDTO.builder()
+          .setUserName("nullUser")
+          .build();
+      when(userDAO.getEntityByUniqueField("userName", userLoginDTO.getUserName()))
+          .thenReturn(null);
+      Assert.assertNotNull(userService.login(userLoginDTO));
+    } catch (FieldException e) {
+      Assert.assertEquals(
+          new FieldException("password", "User or password is wrong, please try again"),
+          e);
+    }
+  }
+
+  @Test
+  public void testLogin_failed_wrongPassword() throws AppException, FieldNotUniqueException {
+    try {
+      UserLoginDTO userLoginDTO = UserLoginDTO.builder()
+          .setPassword("wrongPassword")
+          .build();
+      User user = mock(User.class);
+      when(user.getUserName()).thenReturn("admin");
+      when(user.getPassword()).thenReturn(StringUtil.encodeSHA1("password"));
+      when(userDAO.getEntityByUniqueField("userName", userLoginDTO.getUserName()))
+          .thenReturn(user);
+      Assert.assertNotNull(userService.login(userLoginDTO));
+    } catch (FieldException e) {
+      Assert.assertEquals(
+          new FieldException("password", "User or password is wrong, please try again"),
+          e);
+    }
+  }
+
+  @Test
+  public void testRegister_successful() throws AppException {
+    UserDTO userDTO = UserDTO.builder().build();
+    Assert.assertEquals(userDTO, userService.register(userDTO));
   }
 }
