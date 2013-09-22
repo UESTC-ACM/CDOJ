ebb925235bba61bed64f1c8598a198ae89e51caa
RuinsHe <lyhypacm@gmail.com>
2013-09-22 23:29:31 +0800

    [TEST] get rid of db connection in unit tests.

--- trunk/src/main/java/cn/edu/uestc/acmicpc/oj/controller/base/BaseController.java
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/oj/controller/base/BaseController.java
@@ -1,11 +1,10 @@
 package cn.edu.uestc.acmicpc.oj.controller.base;
 
-import cn.edu.uestc.acmicpc.oj.view.PageInfo;
-import cn.edu.uestc.acmicpc.util.exception.FieldException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 
+import cn.edu.uestc.acmicpc.oj.view.PageInfo;
 import cn.edu.uestc.acmicpc.util.exception.FieldException;
 
 /**
