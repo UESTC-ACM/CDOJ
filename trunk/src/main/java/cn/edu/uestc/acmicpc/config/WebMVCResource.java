7828b7cac51ea71cc719fef72fe881e086e7e996
mzry1992 <muziriyun@gmail.com>
2013-09-22 14:02:06 +0800

    [UPDATE] Use WebMVCResource to get common resource

---
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/config/WebMVCResource.java
@@ -0,0 +1,36 @@
+package cn.edu.uestc.acmicpc.config;
+
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Description
+ */
+public class WebMVCResource {
+
+  public static ViewResolver viewResolver() {
+    InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+
+    viewResolver.setPrefix("/WEB-INF/views/");
+    viewResolver.setSuffix(".jsp");
+
+    return viewResolver;
+  }
+
+  public static HttpMessageConverter<?>[] messageConverters() {
+    HttpMessageConverter<?>[] converters = new HttpMessageConverter<?>[1];
+    FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
+    List<MediaType> mediaTypes = new LinkedList<>();
+    mediaTypes.add(MediaType.APPLICATION_JSON);
+    fastJsonHttpMessageConverter.setSupportedMediaTypes(mediaTypes);
+    converters[0] = fastJsonHttpMessageConverter;
+    return converters;
+  }
+
+}
