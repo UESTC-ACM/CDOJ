cf471556afe905d48c2abeb59fd5d240f6c3deef
mzry1992 <muziriyun@gmail.com>
2013-10-06 20:59:03 +0800

    [UPDATE] Add article controller

--- trunk/src/integration-test/java/cn/edu/uestc/acmicpc/config/IntegrationTestContext.java
+++ trunk/src/integration-test/java/cn/edu/uestc/acmicpc/config/IntegrationTestContext.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.http.HttpRequest;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import cn.edu.uestc.acmicpc.judge.JudgeService;
