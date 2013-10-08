b70d03f4fcea59826f4eeaa9b190a2b3b2c85dcf
mzry1992 <muziriyun@gmail.com>
2013-10-09 01:02:43 +0800

    [UPDATE] Init css

--- src/js/cdoj.js
+++ src/js/cdoj.js
@@ -5,26 +5,27 @@
 var currentUser;
 
 $(function () {
-	currentUser = $('#currentUser')[0].innerHTML;
-	if (currentUser !== undefined) {
+  var $currentUser = $('#currentUser');
+  if ($currentUser.length !== 0) {
+    currentUser = $currentUser[0].innerHTML;
     $('#userAvatar').setAvatar({
       image: 'http://www.acm.uestc.edu.cn/images/akari_small.jpg',
-			size: 120
+      size: 120
     });
-	}
+  }
 
-	$('#cdoj-login-button').setButton({
-		callback: function() {
-			var info=$('#cdoj-login-form').getFormData();
+  $('#cdoj-login-button').setButton({
+    callback: function() {
+      var info=$('#cdoj-login-form').getFormData();
 
-			jsonPost('/user/login', info, function(data) {
-				$('#cdoj-login-form').formValidate({
-					result: data,
-					onSuccess: function() {
-						window.location.reload();
-					}
-				});
-			});
-		}
-	});
+      jsonPost('/user/login', info, function(data) {
+        $('#cdoj-login-form').formValidate({
+          result: data,
+          onSuccess: function() {
+            window.location.reload();
+          }
+        });
+      });
+    }
+  });
 });
