c0bb5d537b4ddfab4c1c02df0ba153dba61112ed
mzry1992 <muziriyun@gmail.com>
2013-10-11 22:02:22 +0800

    [UPDATE] Check login status before submit

--- src/js/cdoj.problem.problemShow.js
+++ src/js/cdoj.problem.problemShow.js
@@ -17,33 +17,43 @@ $(document).ready(function () {
     statusList.autoRefresh('off');
   });
 
-  var currentProblem = $('#problem_title').attr('value');
-  //Blind Submit
-  $('#submitCode').click(function () {
-    var info = {
-      'codeContent': null,
-      'languageId': null,
-      'contestId': null,
-      'problemId': currentProblem
-    };
-    //Get code content
-    info.codeContent = $('#codeContent').val();
-    //Get language Id
-    info.languageId = $('#languageSelector').find('.active').attr('value');
+  if (currentUser === undefined || currentUser === null) {
+    $('a[href="#tab-problem-submit"]')
+        .attr('data-toggle', '')
+        .parent()
+        .addClass('pure-menu-disabled')
+        .tooltip({
+          title: 'Please login first!'
+        });
+  } else {
+    var currentProblem = $('#problem_title').attr('value');
+    //Blind Submit
+    $('#submitCode').click(function () {
+      var info = {
+        'codeContent': null,
+        'languageId': null,
+        'contestId': null,
+        'problemId': currentProblem
+      };
+      //Get code content
+      info.codeContent = $('#codeContent').val();
+      //Get language Id
+      info.languageId = $('#languageSelector').find('.active').attr('value');
 
-    jsonPost('/status/submit', info, function (data) {
-      if (data.result == 'error')
-        alert(data.error_msg);
-      else if (data.result == 'success') {
-        $('#problem-show-tab').find('a[href="#tab-problem-status"]').tab('show');
+      jsonPost('/status/submit', info, function (data) {
+        if (data.result == 'error')
+          alert(data.error_msg);
+        else if (data.result == 'success') {
+          $('#problem-show-tab').find('a[href="#tab-problem-status"]').tab('show');
 
-        //TODO change it like PC^2
-      } else {
-        console.log(data);
-        alert('Please login first!');
-        //$("#loginModal").modal();
-      }
+          //TODO change it like PC^2
+        } else {
+          console.log(data);
+          alert('Please login first!');
+          //$("#loginModal").modal();
+        }
+      });
     });
-  });
+  }
 
 });
