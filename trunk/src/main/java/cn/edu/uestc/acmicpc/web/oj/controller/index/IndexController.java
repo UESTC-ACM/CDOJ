61cb86ac3eea1e78b0346f78e10bea4db62666de
mzry1992 <muziriyun@gmail.com>
2013-10-16 00:25:27 +0800

    [UPDATE] Set common model attribute in base controller

--- trunk/src/main/java/cn/edu/uestc/acmicpc/web/oj/controller/index/IndexController.java
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/web/oj/controller/index/IndexController.java
@@ -1,14 +1,11 @@
 package cn.edu.uestc.acmicpc.web.oj.controller.index;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import cn.edu.uestc.acmicpc.util.annotation.LoginPermit;
+import cn.edu.uestc.acmicpc.web.oj.controller.base.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import cn.edu.uestc.acmicpc.web.oj.controller.base.BaseController;
-import cn.edu.uestc.acmicpc.service.iface.DepartmentService;
-import cn.edu.uestc.acmicpc.util.annotation.LoginPermit;
 
 /**
  * Controller for home page.
@@ -17,18 +14,10 @@ import cn.edu.uestc.acmicpc.util.annotation.LoginPermit;
 @RequestMapping("/")
 public class IndexController extends BaseController {
 
-  private DepartmentService departmentService;
-
-  @Autowired
-  public void setDepartmentService(DepartmentService departmentService) {
-    this.departmentService = departmentService;
-  }
-
   @RequestMapping(method = RequestMethod.GET)
   @LoginPermit(NeedLogin = false)
   public String index(ModelMap model) {
     model.put("message", "home page.");
-    model.put("departmentList", departmentService.getDepartmentList());
     return "index/index";
   }
 
--- trunk/src/main/java/cn/edu/uestc/acmicpc/web/oj/controller/user/UserController.java
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/web/oj/controller/user/UserController.java
@@ -1,14 +1,16 @@
 package cn.edu.uestc.acmicpc.web.oj.controller.user;
 
-import java.sql.Timestamp;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
-
+import cn.edu.uestc.acmicpc.db.condition.impl.UserCondition;
+import cn.edu.uestc.acmicpc.db.dto.impl.user.*;
+import cn.edu.uestc.acmicpc.db.dto.impl.userSerialKey.UserSerialKeyDTO;
+import cn.edu.uestc.acmicpc.service.iface.*;
+import cn.edu.uestc.acmicpc.util.Global;
+import cn.edu.uestc.acmicpc.util.StringUtil;
+import cn.edu.uestc.acmicpc.util.annotation.LoginPermit;
+import cn.edu.uestc.acmicpc.util.exception.AppException;
+import cn.edu.uestc.acmicpc.util.exception.FieldException;
+import cn.edu.uestc.acmicpc.web.oj.controller.base.BaseController;
+import cn.edu.uestc.acmicpc.web.view.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -18,29 +20,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import cn.edu.uestc.acmicpc.db.condition.impl.UserCondition;
-import cn.edu.uestc.acmicpc.db.dto.impl.user.UserActivateDTO;
-import cn.edu.uestc.acmicpc.db.dto.impl.user.UserCenterDTO;
-import cn.edu.uestc.acmicpc.db.dto.impl.user.UserDTO;
-import cn.edu.uestc.acmicpc.db.dto.impl.user.UserEditDTO;
-import cn.edu.uestc.acmicpc.db.dto.impl.user.UserLoginDTO;
-import cn.edu.uestc.acmicpc.db.dto.impl.user.UserRegisterDTO;
-import cn.edu.uestc.acmicpc.db.dto.impl.user.UserSummaryDTO;
-import cn.edu.uestc.acmicpc.db.dto.impl.userSerialKey.UserSerialKeyDTO;
-import cn.edu.uestc.acmicpc.service.iface.DepartmentService;
-import cn.edu.uestc.acmicpc.service.iface.EmailService;
-import cn.edu.uestc.acmicpc.service.iface.GlobalService;
-import cn.edu.uestc.acmicpc.service.iface.ProblemService;
-import cn.edu.uestc.acmicpc.service.iface.StatusService;
-import cn.edu.uestc.acmicpc.service.iface.UserSerialKeyService;
-import cn.edu.uestc.acmicpc.service.iface.UserService;
-import cn.edu.uestc.acmicpc.util.Global;
-import cn.edu.uestc.acmicpc.util.StringUtil;
-import cn.edu.uestc.acmicpc.util.annotation.LoginPermit;
-import cn.edu.uestc.acmicpc.util.exception.AppException;
-import cn.edu.uestc.acmicpc.util.exception.FieldException;
-import cn.edu.uestc.acmicpc.web.oj.controller.base.BaseController;
-import cn.edu.uestc.acmicpc.web.view.PageInfo;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * User controller
@@ -63,8 +49,6 @@ import cn.edu.uestc.acmicpc.web.view.PageInfo;
 public class UserController extends BaseController {
 
   private UserService userService;
-  private GlobalService globalService;
-  private DepartmentService departmentService;
   private ProblemService problemService;
   private StatusService statusService;
   private UserSerialKeyService userSerialKeyService;
@@ -73,16 +57,6 @@ public class UserController extends BaseController {
   @Autowired
   public void setUserService(UserService userService) {
     this.userService = userService;
-  }
-
-  @Autowired
-  public void setGlobalService(GlobalService globalService) {
-    this.globalService = globalService;
-  }
-
-  @Autowired
-  public void setDepartmentService(DepartmentService departmentService) {
-    this.departmentService = departmentService;
   }
 
   @Autowired
@@ -257,8 +231,6 @@ public class UserController extends BaseController {
   @RequestMapping("list")
   @LoginPermit(NeedLogin = false)
   public String list(ModelMap model) {
-    model.put("departmentList", departmentService.getDepartmentList());
-    model.put("authenticationTypeList", globalService.getAuthenticationTypeList());
     return "user/userList";
   }
 
@@ -317,7 +289,6 @@ public class UserController extends BaseController {
         throw new AppException("No such user!");
       }
 
-      model.put("departmentList", departmentService.getDepartmentList());
       model.put("targetUser", userCenterDTO);
     } catch (AppException e) {
       return "error/404";
