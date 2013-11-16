6f3b4122dd7f0884395996af23af4a061e7f64ba
RuinsHe <lyhypacm@gmail.com>
2013-11-16 17:14:30 +0800

    format Global for frendly reading.

--- trunk/src/main/java/cn/edu/uestc/acmicpc/util/Global.java
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/util/Global.java
@@ -32,15 +32,25 @@ public class Global {
   public static final int USER_SERIAL_KEY_LENGTH = 128;
 
   public enum OnlineJudgeReturnType {
-    OJ_WAIT("Queuing"), OJ_AC("Accepted"), OJ_PE("Presentation Error on test $case"), OJ_TLE(
-        "Time Limit Exceeded on test $case"), OJ_MLE("Memory Limit Exceeded on test $case"), OJ_WA(
-        "Wrong Answer on test $case"), OJ_OLE("Output Limit Exceeded on test $case"), OJ_CE(
-        "Compilation Error"), OJ_RE_SEGV("Runtime Error on test $case"), OJ_RE_FPE(
-        "Runtime Error on test $case"), OJ_RE_BUS("Runtime Error on test $case"), OJ_RE_ABRT(
-        "Runtime Error on test $case"), OJ_RE_UNKNOWN("Runtime Error on test $case"), OJ_RF(
-        "Restricted Function on test $case"), OJ_SE("System Error on test $case"), OJ_RE_JAVA(
-        "Runtime Error on test $case"), OJ_JUDGING("Queuing"), OJ_RUNNING("Running on test $case"),
-    OJ_REJUDGING("Queuing");
+    OJ_WAIT("Queuing"),                             // 0
+    OJ_AC("Accepted"),                              // 1
+    OJ_PE("Presentation Error on test $case"),      // 2
+    OJ_TLE("Time Limit Exceeded on test $case"),    // 3
+    OJ_MLE("Memory Limit Exceeded on test $case"),  // 4
+    OJ_WA("Wrong Answer on test $case"),            // 5
+    OJ_OLE("Output Limit Exceeded on test $case"),  // 6
+    OJ_CE("Compilation Error"),                     // 7
+    OJ_RE_SEGV("Runtime Error on test $case"),      // 8
+    OJ_RE_FPE("Runtime Error on test $case"),       // 9
+    OJ_RE_BUS("Runtime Error on test $case"),       // 10
+    OJ_RE_ABRT("Runtime Error on test $case"),      // 11
+    OJ_RE_UNKNOWN("Runtime Error on test $case"),   // 12
+    OJ_RF( "Restricted Function on test $case"),    // 13
+    OJ_SE("System Error on test $case"),            // 14
+    OJ_RE_JAVA("Runtime Error on test $case"),      // 15
+    OJ_JUDGING("Queuing"),                          // 16
+    OJ_RUNNING("Running on test $case"),            // 17
+    OJ_REJUDGING("Queuing");                        // 18
 
     private String description;
 
