2c86af2239797ec3db46476c327be74904a98c78
RuinsHe <lyhypacm@gmail.com>
2014-01-27 23:18:26 +0800

    [TEST] add show/list test case for ProblemController.
    BUG: 20
    Change-Id: I021f97018d76532c271a81371eb97a44a9f2ee0f

--- trunk/src/test/java/cn/edu/uestc/acmicpc/web/oj/controller/ProblemControllerTest.java
+++ trunk/src/test/java/cn/edu/uestc/acmicpc/web/oj/controller/ProblemControllerTest.java
@@ -2,11 +2,17 @@ package cn.edu.uestc.acmicpc.web.oj.controller;
 
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.UUID;
 
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.ContextConfiguration;
@@ -16,7 +22,9 @@ import org.testng.annotations.Test;
 
 import cn.edu.uestc.acmicpc.config.TestContext;
 import cn.edu.uestc.acmicpc.config.WebMVCConfig;
+import cn.edu.uestc.acmicpc.db.condition.impl.StatusCondition;
 import cn.edu.uestc.acmicpc.db.dto.impl.problem.ProblemShowDTO;
+import cn.edu.uestc.acmicpc.util.settings.Global.OnlineJudgeResultType;
 import cn.edu.uestc.acmicpc.web.oj.controller.problem.ProblemController;
 
 /**
@@ -27,6 +35,7 @@ import cn.edu.uestc.acmicpc.web.oj.controller.problem.ProblemController;
 public class ProblemControllerTest extends ControllerTest {
 
   private final String URL_SHOW = "/problem/show";
+  private final String URL_LIST = "/problem/list";
 
   @Autowired private ProblemController problemController;
 
@@ -42,7 +51,35 @@ public class ProblemControllerTest extends ControllerTest {
   public void testShow_successful() throws Exception {
     ProblemShowDTO problemShowDTO = ProblemShowDTO.builder().setProblemId(1000).build();
     when(problemService.getProblemShowDTO(anyInt())).thenReturn(problemShowDTO);
-    mockMvc.perform(post(URL_SHOW + "/{problemId}", problemShowDTO.getProblemId())).andExpect(status().isOk());
+    when(statusService.count(Mockito.<StatusCondition>any())).thenReturn(100L);
+    Map<OnlineJudgeResultType, Long> problemStatistic = new TreeMap<>();
+    for (OnlineJudgeResultType type : OnlineJudgeResultType.values()) {
+      if (type == OnlineJudgeResultType.OJ_WAIT) {
+        continue;
+      }
+      problemStatistic.put(type, 100L);
+    }
+    mockMvc.perform(post(URL_SHOW + "/{problemId}", problemShowDTO.getProblemId()))
+        .andExpect(status().isOk())
+        .andExpect(view().name("problem/problemShow"))
+        .andExpect(model().attribute("problemStatistic", problemStatistic))
+        .andExpect(model().attribute("targetProblem", problemShowDTO))
+        .andExpect(model().attribute("brToken", "\n"))
+        .andExpect(model().attribute("languageList", languageList));
   }
 
+  @Test
+  public void testShow_problemNotFound() throws Exception {
+    when(problemService.getProblemShowDTO(anyInt())).thenReturn(null);
+    mockMvc.perform(post(URL_SHOW + "/{problemId}", 1000))
+        .andExpect(status().isOk())
+        .andExpect(view().name("error/404"));
+  }
+
+  @Test
+  public void testList() throws Exception {
+    mockMvc.perform(get(URL_LIST))
+        .andExpect(status().isOk())
+        .andExpect(view().name("problem/problemList"));
+  }
 }
