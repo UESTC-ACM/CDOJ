f7cb894f2a4cce60c25b0f48670bf033a7fb05cd
RuinsHe <lyhypacm@gmail.com>
2014-01-27 21:18:52 +0800

    [TEST] modify test listener, only shows class name when testing.
    Change-Id: I28f8e3f29f3297b2c1f3da464c1eab9e6ee4b23e

--- trunk/src/test/java/cn/edu/uestc/acmicpc/config/TestListener.java
+++ trunk/src/test/java/cn/edu/uestc/acmicpc/config/TestListener.java
@@ -1,56 +1,45 @@
 package cn.edu.uestc.acmicpc.config;
 
+import cn.edu.uestc.acmicpc.util.log.LogConstants;
 import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
 
-import cn.edu.uestc.acmicpc.util.log.LogConstants;
-
+/**
+ * Test listener to show colorful message in console.
+ */
 public class TestListener extends TestListenerAdapter {
 
   @Override
-  public void onStart(ITestContext testContext) {
-    System.out.print("Test: ");
-    System.out.print(LogConstants.WARN_COLOR);
-    System.out.print(testContext.getName());
-    System.out.println(LogConstants.END_COLOUR);
-  }
+  public void onStart(ITestContext testContext) {}
 
   @Override
   public void onFinish(ITestContext testContext) {
-  }
-
-  @Override
-  public void onTestSuccess(ITestResult result) {
     StringBuilder builder = new StringBuilder();
-    builder.append(LogConstants.INFO_COLOR);
-    builder.append("PASSED: ");
+    if (testContext.getFailedTests().size() != 0) {
+      builder.append(LogConstants.FATAL_COLOR);
+      builder.append("FAILED: ");
+    } else if (testContext.getFailedButWithinSuccessPercentageTests().size() != 0) {
+      builder.append(LogConstants.WARN_COLOR);
+      builder.append("FLAKY: ");
+    } else {
+      builder.append(LogConstants.INFO_COLOR);
+      builder.append("PASSED: ");
+    }
     builder.append(LogConstants.END_COLOUR);
-    builder.append(result.getTestClass().getRealClass().getSimpleName());
-    builder.append("#");
-    builder.append(result.getName());
+    builder.append(testContext.getName());
     System.out.println(builder.toString());
   }
 
   @Override
-  public void onTestFailure(ITestResult result) {
-    StringBuilder builder = new StringBuilder();
-    builder.append(LogConstants.FATAL_COLOR);
-    builder.append("FAILED: ");
-    builder.append(LogConstants.END_COLOUR);
-    builder.append(result.getTestClass().getRealClass().getSimpleName());
-    builder.append("#");
-    builder.append(result.getName());
-    System.out.println(builder.toString());
-  }
+  public void onTestFailedButWithinSuccessPercentage(ITestResult tr) {}
 
   @Override
-  public void onTestSkipped(ITestResult result) {
-    StringBuilder builder = new StringBuilder();
-    builder.append("SKIPPED: ");
-    builder.append(result.getTestClass().getRealClass().getSimpleName());
-    builder.append("#");
-    builder.append(result.getName());
-    System.out.println(builder.toString());
-  }
+  public void onTestSkipped(ITestResult tr) {}
+
+  @Override
+  public void onTestSuccess(ITestResult tr) {}
+
+  @Override
+  public void onTestStart(ITestResult result) {}
 }
