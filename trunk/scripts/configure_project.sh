0dbdaa3c22716c8853e3844e0315bb5a3d497d8a
RuinsHe <lyhypacm@gmail.com>
2014-02-13 21:41:38 +0800

    set host name directly
    Change-Id: Ic49336cbb17161edc2f2370e82714345ab1076ca

--- trunk/scripts/configure_project.sh
+++ trunk/scripts/configure_project.sh
@@ -3,23 +3,16 @@
 # configure project
 #
 # parameters:
-# total: 2
+# total: 1
 # $1 password for db
-# $2 host name
 
 password=root
-hostname=http:\\/\\/127.0.0.1:8080
 
 if [ $# -ge 1 ]; then
   password=$1
 fi
 
-if [ $# -ge 2 ]; then
-  hostname=$2
-fi
-
 sed -i "s/<db.password>root<\/db.password>/<db.password>${password}<\/db.password>/g" pom.xml
 sed -i "s/db.password=root/db.password=${password}/g" src/main/resources/resources.properties
 sed -i "s/db.password=root/db.password=${password}/g" src/test/resources/resources.properties
-sed -i "s/http:\/\/127.0.0.1:8080/${hostname}/g" src/main/resources/settings.xml
 
