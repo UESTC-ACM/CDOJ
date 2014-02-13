6cc41bed8ddc7011bad9e2b7620190a3e990f9e9
RuinsHe <lyhypacm@gmail.com>
2014-02-13 21:26:47 +0800

    modify configure script
    Change-Id: I40db201c82510fcece56f45257a6118b6771e9a7

--- trunk/scripts/configure_project.sh
+++ trunk/scripts/configure_project.sh
@@ -21,6 +21,5 @@ fi
 sed -i "s/<db.password>root<\/db.password>/<db.password>${password}<\/db.password>/g" pom.xml
 sed -i "s/db.password=root/db.password=${password}/g" src/main/resources/resources.properties
 sed -i "s/db.password=root/db.password=${password}/g" src/test/resources/resources.properties
-sed "s/http:\/\/127.0.0.1:8080/${hostname}/g" src/main/resources/settings.xml > settings.xml
-mv settings.xml src/main/resources/settings.xml
+sed -i "s/http:\/\/127.0.0.1:8080/${hostname}/g" src/main/resources/settings.xml
 
