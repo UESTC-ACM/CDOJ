86a8925c8171362d46a2318879b5fb82c8288f88
Jianjin Fan <fanjianjin@gmail.com>
2014-02-18 15:09:53 +0800

    [UPDATE] change menu-li's hover color to @cdoj-blue0 for unity.

--- dist/js/cdoj.angular.js
+++ dist/js/cdoj.angular.js
@@ -25359,18 +25359,19 @@ angular.module("template/typeahead/typeahead-popup.html", []).run(["$templateCac
         return $scope.problemList.splice(index, 1);
       };
       return $scope.submit = function() {
-        var contestEditDTO,
-          _this = this;
+        var contestEditDTO;
         contestEditDTO = angular.copy($scope.contest);
         contestEditDTO.time = Date.create(contestEditDTO.time).getTime();
         console.log($scope.contest);
-        return $http.post("/contest/edit", contestEditDTO).success(function(data) {
-          if (data.result === "success") {
-            return $window.location.href = "/contest/show/" + data.contestId;
-          } else {
-            return $window.alert(data.error_msg);
-          }
-        });
+        return $http.post("/contest/edit", contestEditDTO).success((function(_this) {
+          return function(data) {
+            if (data.result === "success") {
+              return $window.location.href = "/contest/show/" + data.contestId;
+            } else {
+              return $window.alert(data.error_msg);
+            }
+          };
+        })(this));
       };
     }
   ]);
@@ -25393,14 +25394,15 @@ angular.module("template/typeahead/typeahead-popup.html", []).run(["$templateCac
         return $scope.condition["currentPage"] = null;
       };
       $scope.refresh = function() {
-        var condition,
-          _this = this;
+        var condition;
         if ($scope.requestUrl !== 0) {
           condition = angular.copy($scope.condition);
-          return $http.post($scope.requestUrl, condition).then(function(response) {
-            $scope.list = response.data.list;
-            return $scope.pageInfo = response.data.pageInfo;
-          });
+          return $http.post($scope.requestUrl, condition).then((function(_this) {
+            return function(response) {
+              $scope.list = response.data.list;
+              return $scope.pageInfo = response.data.pageInfo;
+            };
+          })(this));
         }
       };
       $rootScope.$watch("currentUser", function() {
@@ -25791,6 +25793,7 @@ angular.module("template/typeahead/typeahead-popup.html", []).run(["$templateCac
     }
   ]);
 
+
   /*
   cdoj.directive("autoFill", ["$timeout",
     ($timeout)->
@@ -25805,8 +25808,7 @@ angular.module("template/typeahead/typeahead-popup.html", []).run(["$templateCac
             $timeout($scope.check, 300)
           $scope.check()
   ])
-  */
-
+   */
 
   cdoj.directive("equals", function() {
     return {
@@ -25956,14 +25958,15 @@ angular.module("template/typeahead/typeahead-popup.html", []).run(["$templateCac
       controller: [
         "$scope", "$http", function($scope, $http) {
           return $scope.editVisible = function() {
-            var queryString,
-              _this = this;
+            var queryString;
             queryString = "/contest/operator/" + $scope.contestId + "/isVisible/" + (!$scope.isVisible);
-            return $http.post(queryString).then(function(response) {
-              if (response.data.result === "success") {
-                return $scope.isVisible = !$scope.isVisible;
-              }
-            });
+            return $http.post(queryString).then((function(_this) {
+              return function(response) {
+                if (response.data.result === "success") {
+                  return $scope.isVisible = !$scope.isVisible;
+                }
+              };
+            })(this));
           };
         }
       ],
@@ -25984,10 +25987,11 @@ angular.module("template/typeahead/typeahead-popup.html", []).run(["$templateCac
     return {
       restrict: "A",
       link: function($scope, $element) {
-        var _this = this;
-        return $element.find('form').click(function(e) {
-          return e.stopPropagation();
-        });
+        return $element.find('form').click((function(_this) {
+          return function(e) {
+            return e.stopPropagation();
+          };
+        })(this));
       }
     };
   });
@@ -26124,14 +26128,15 @@ angular.module("template/typeahead/typeahead-popup.html", []).run(["$templateCac
       controller: [
         "$scope", "$http", function($scope, $http) {
           return $scope.editVisible = function() {
-            var queryString,
-              _this = this;
+            var queryString;
             queryString = "/problem/operator/" + $scope.problemId + "/isVisible/" + (!$scope.isVisible);
-            return $http.post(queryString).then(function(response) {
-              if (response.data.result === "success") {
-                return $scope.isVisible = !$scope.isVisible;
-              }
-            });
+            return $http.post(queryString).then((function(_this) {
+              return function(response) {
+                if (response.data.result === "success") {
+                  return $scope.isVisible = !$scope.isVisible;
+                }
+              };
+            })(this));
           };
         }
       ],
