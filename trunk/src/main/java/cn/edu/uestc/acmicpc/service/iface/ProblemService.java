effd33672a41ebd29f3758804837e57e5493bfbd
Jianjin Fan <fanjianjin@gmail.com>
2014-03-04 18:13:27 +0800

    create problems from a list of problemDTOs

--- trunk/src/main/java/cn/edu/uestc/acmicpc/service/iface/ProblemService.java
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/service/iface/ProblemService.java
@@ -7,6 +7,7 @@ import cn.edu.uestc.acmicpc.db.entity.Problem;
 import cn.edu.uestc.acmicpc.util.exception.AppException;
 import cn.edu.uestc.acmicpc.web.dto.PageInfo;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -100,6 +101,15 @@ public interface ProblemService extends DatabaseService<Problem, Integer> {
       throws AppException;
 
   /**
+   * Create problems from by problemDTOs.
+   *
+   * @return List of problemDTOs with problemId.
+   * @param problemDTOS a series of {@link cn.edu.uestc.acmicpc.db.dto.impl.problem.ProblemDTO} entity.
+   *
+   */
+  public ArrayList<ProblemDTO> createProblems(ArrayList<ProblemDTO> problemDTOs) throws AppException;
+
+  /**
    * Check whether a problem exists.
    *
    * @param problemId problem's id.
@@ -107,4 +117,5 @@ public interface ProblemService extends DatabaseService<Problem, Integer> {
    * @throws AppException
    */
   public Boolean checkProblemExists(Integer problemId) throws AppException;
+
 }
