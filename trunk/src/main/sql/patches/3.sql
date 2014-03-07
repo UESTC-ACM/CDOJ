6bad228570e80b5c1d9ea074d552853254ddaa07
mzry1992 <muziriyun@gmail.com>
2014-03-07 16:55:13 +0800

    [UPDATE] Create team, contestTeam, teamUser table, and create patch script

---
+++ trunk/src/main/sql/patches/3.sql
@@ -0,0 +1,54 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+ALTER TABLE `uestcoj`.`user`  ADD COLUMN `name` VARCHAR(50) NOT NULL  AFTER `motto` , ADD COLUMN `sex` INT(11) NOT NULL  AFTER `name` , ADD COLUMN `grade` INT(11) NOT NULL  AFTER `sex` , ADD COLUMN `phone` VARCHAR(45) NOT NULL  AFTER `grade` , ADD COLUMN `size` INT(11) NOT NULL  AFTER `phone` ;
+
+CREATE  TABLE IF NOT EXISTS `uestcoj`.`teamUser` (
+  `teamUserId` INT(11) NOT NULL ,
+  `userId` INT(11) NOT NULL ,
+  `teamId` INT(11) NOT NULL ,
+  PRIMARY KEY (`teamUserId`) ,
+  INDEX `teamId_idx` (`teamId` ASC) ,
+  INDEX `userId_idx` (`userId` ASC) ,
+  CONSTRAINT `FK_team_teamId_on_team`
+    FOREIGN KEY (`teamId` )
+    REFERENCES `uestcoj`.`team` (`teamId` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `FK_team_userId_on_user`
+    FOREIGN KEY (`userId` )
+    REFERENCES `uestcoj`.`user` (`userId` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
+
+CREATE  TABLE IF NOT EXISTS `uestcoj`.`contestTeam` (
+  `contestTeamId` INT(11) NOT NULL ,
+  `contestId` INT(11) NOT NULL ,
+  `teamId` INT(11) NOT NULL ,
+  PRIMARY KEY (`contestTeamId`) ,
+  INDEX `FK_contestTeam_contestId_on_contest_idx` (`contestId` ASC) ,
+  INDEX `FK_contestTeam_teamId_on_team_idx` (`teamId` ASC) ,
+  CONSTRAINT `FK_contestTeam_contestId_on_contest`
+    FOREIGN KEY (`contestId` )
+    REFERENCES `uestcoj`.`contest` (`contestId` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `FK_contestTeam_teamId_on_team`
+    FOREIGN KEY (`teamId` )
+    REFERENCES `uestcoj`.`team` (`teamId` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
+
+DROP TABLE IF EXISTS `uestcoj`.`contestTeamInfo` ;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
