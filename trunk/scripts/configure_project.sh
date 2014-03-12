af25eb8a5dc185a7f5b668d08e3847151824edf8
RuinsHe <lyhypacm@gmail.com>
2014-03-12 16:31:37 +0800

    delete maven configuration file and use gradle instead.

--- trunk/scripts/configure_project.sh
+++ trunk/scripts/configure_project.sh
@@ -3,16 +3,16 @@
 # configure project
 #
 # parameters:
-# total: 1
-# $1 password for db
+# total: 4
+# $1 original file
+# $2 destination file
+# $3 user for db
+# $4 password for db
 
-password=root
-
-if [ $# -ge 1 ]; then
-  password=$1
+if [ $# -ne 4 ]; then
+  exit 1
 fi
 
-sed -i "s/<db.password>root<\/db.password>/<db.password>${password}<\/db.password>/g" pom.xml
-sed -i "s/db.password=root/db.password=${password}/g" src/main/resources/resources.properties
-sed -i "s/db.password=root/db.password=${password}/g" src/test/resources/resources.properties
-
+cp $1 $2
+echo "db.username=$3" >> $2
+echo "db.password=$4" >> $2
