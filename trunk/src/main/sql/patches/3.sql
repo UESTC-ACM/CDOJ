a614672672ae04c4bcce4deece63cbe34d82588d
mzry1992 <muziriyun@gmail.com>
2014-03-14 14:29:47 +0800

    [UPDATE] Merge patch 4.sql and 3.sql

--- trunk/src/main/sql/patches/3.sql
+++ trunk/src/main/sql/patches/3.sql
@@ -1,8 +1,17 @@
+-- Add tables used in contest registrer system
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-ALTER TABLE `uestcoj`.`user`  ADD COLUMN `name` VARCHAR(50) NOT NULL DEFAULT '' AFTER `motto` , ADD COLUMN `sex` INT(11) NOT NULL DEFAULT 0  AFTER `name` , ADD COLUMN `grade` INT(11) NOT NULL DEFAULT 0  AFTER `sex` , ADD COLUMN `phone` VARCHAR(45) NOT NULL DEFAULT ''  AFTER `grade` , ADD COLUMN `size` INT(11) NOT NULL DEFAULT 0 AFTER `phone` ;
+ALTER TABLE `uestcoj`.`user` 
+  ADD COLUMN `name` VARCHAR(50) NOT NULL DEFAULT '' AFTER `motto` , 
+  ADD COLUMN `sex` INT(11) NOT NULL DEFAULT 0  AFTER `name` , 
+  ADD COLUMN `grade` INT(11) NOT NULL DEFAULT 0  AFTER `sex` , 
+  ADD COLUMN `phone` VARCHAR(45) NOT NULL DEFAULT ''  AFTER `grade` , 
+  ADD COLUMN `size` INT(11) NOT NULL DEFAULT 0 AFTER `phone` ;
+
+ALTER TABLE `uestcoj`.`contestUser` 
+  ADD COLUMN `comment` VARCHAR(255) NOT NULL AFTER `OPTLOCK`;
 
 CREATE  TABLE IF NOT EXISTS `uestcoj`.`team` (
   `teamId` INT NOT NULL AUTO_INCREMENT ,
@@ -48,6 +57,8 @@ CREATE  TABLE IF NOT EXISTS `uestcoj`.`contestTeam` (
   `contestTeamId` INT(11) NOT NULL AUTO_INCREMENT ,
   `contestId` INT(11) NOT NULL ,
   `teamId` INT(11) NOT NULL ,
+  `status` TINYINT(4) NOT NULL,  -- 0 - wait for validating, 1 - accepted, 2 - refused 
+  `comment` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`contestTeamId`) ,
   INDEX `FK_contestTeam_contestId_on_contest_idx` (`contestId` ASC) ,
   INDEX `FK_contestTeam_teamId_on_team_idx` (`teamId` ASC) ,
@@ -67,7 +78,6 @@ DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_bin;
 
 DROP TABLE IF EXISTS `uestcoj`.`contestTeamInfo` ;
-
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
