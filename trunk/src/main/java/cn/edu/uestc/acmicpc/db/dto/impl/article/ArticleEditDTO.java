558aeb4e92ae2bc7f68505d4c339327d1a726dd9
Jianjin Fan <fanjianjin@gmail.com>
2014-03-18 10:28:48 +0800

    add equals and hashCode methods for every DTO about article.

--- trunk/src/main/java/cn/edu/uestc/acmicpc/db/dto/impl/article/ArticleEditDTO.java
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/db/dto/impl/article/ArticleEditDTO.java
@@ -55,6 +55,31 @@ public class ArticleEditDTO implements BaseDTO<Article> {
     this.content = content;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    ArticleEditDTO that = (ArticleEditDTO) o;
+
+    if (action != null ? !action.equals(that.action) : that.action != null) return false;
+    if (articleId != null ? !articleId.equals(that.articleId) : that.articleId != null)
+      return false;
+    if (content != null ? !content.equals(that.content) : that.content != null) return false;
+    if (title != null ? !title.equals(that.title) : that.title != null) return false;
+
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = articleId != null ? articleId.hashCode() : 0;
+    result = 31 * result + (action != null ? action.hashCode() : 0);
+    result = 31 * result + (title != null ? title.hashCode() : 0);
+    result = 31 * result + (content != null ? content.hashCode() : 0);
+    return result;
+  }
+
   public static Builder builder() {
     return new Builder();
   }
