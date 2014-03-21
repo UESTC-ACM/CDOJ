f943846bb4d47de684ec585e85d43e71355864c0
mzry1992 <muziriyun@gmail.com>
2014-03-21 14:34:04 +0800

    [UPDATE] Add comment skeleton

--- trunk/src/main/java/cn/edu/uestc/acmicpc/db/dto/impl/article/ArticleEditDTO.java
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/db/dto/impl/article/ArticleEditDTO.java
@@ -11,14 +11,18 @@ public class ArticleEditDTO implements BaseDTO<Article> {
   public ArticleEditDTO() {
   }
 
-  private ArticleEditDTO(Integer articleId, String action, String title, String content,
-                         String userName, Integer type) {
+  public ArticleEditDTO(Integer articleId, String action, String title, String content,
+                        String userName, Integer type, Integer problemId, Integer contestId,
+                        Integer parentId) {
     this.articleId = articleId;
     this.action = action;
     this.title = title;
     this.content = content;
     this.userName = userName;
     this.type = type;
+    this.problemId = problemId;
+    this.contestId = contestId;
+    this.parentId = parentId;
   }
 
   private Integer articleId;
@@ -27,6 +31,33 @@ public class ArticleEditDTO implements BaseDTO<Article> {
   private String content;
   private String userName;
   private Integer type;
+  private Integer problemId;
+  private Integer contestId;
+  private Integer parentId;
+
+  public Integer getProblemId() {
+    return problemId;
+  }
+
+  public void setProblemId(Integer problemId) {
+    this.problemId = problemId;
+  }
+
+  public Integer getContestId() {
+    return contestId;
+  }
+
+  public void setContestId(Integer contestId) {
+    this.contestId = contestId;
+  }
+
+  public Integer getParentId() {
+    return parentId;
+  }
+
+  public void setParentId(Integer parentId) {
+    this.parentId = parentId;
+  }
 
   public Integer getArticleId() {
     return articleId;
@@ -105,6 +136,15 @@ public class ArticleEditDTO implements BaseDTO<Article> {
     if (type != null ? !type.equals(that.type) : that.type != null) {
       return false;
     }
+    if (problemId != null ? !problemId.equals(that.problemId) : that.problemId != null) {
+      return false;
+    }
+    if (contestId != null ? !contestId.equals(that.contestId) : that.contestId != null) {
+      return false;
+    }
+    if (articleId != null ? !articleId.equals(that.articleId) : that.articleId != null) {
+      return false;
+    }
 
     return true;
   }
@@ -117,6 +157,9 @@ public class ArticleEditDTO implements BaseDTO<Article> {
     result = 31 * result + (content != null ? content.hashCode() : 0);
     result = 31 * result + (userName != null ? userName.hashCode() : 0);
     result = 31 * result + (type != null ? type.hashCode() : 0);
+    result = 31 * result + (problemId != null ? problemId.hashCode() : 0);
+    result = 31 * result + (contestId != null ? contestId.hashCode() : 0);
+    result = 31 * result + (articleId != null ? articleId.hashCode() : 0);
     return result;
   }
 
@@ -130,7 +173,8 @@ public class ArticleEditDTO implements BaseDTO<Article> {
     }
 
     public ArticleEditDTO build() {
-      return new ArticleEditDTO(articleId, action, title, content, userName, type);
+      return new ArticleEditDTO(articleId, action, title, content, userName, type, problemId,
+          contestId, parentId);
     }
 
     private Integer articleId;
@@ -139,6 +183,9 @@ public class ArticleEditDTO implements BaseDTO<Article> {
     private String content;
     private String userName;
     private Integer type;
+    private Integer problemId;
+    private Integer contestId;
+    private Integer parentId;
 
     public Integer getArticleId() {
       return articleId;
@@ -191,6 +238,33 @@ public class ArticleEditDTO implements BaseDTO<Article> {
 
     public Builder setType(Integer type) {
       this.type = type;
+      return this;
+    }
+
+    public Integer getProblemId() {
+      return problemId;
+    }
+
+    public Builder setProblemId(Integer problemId) {
+      this.problemId = problemId;
+      return this;
+    }
+
+    public Integer getContestId() {
+      return contestId;
+    }
+
+    public Builder setContestId(Integer contestId) {
+      this.contestId = contestId;
+      return this;
+    }
+
+    public Integer getParentId() {
+      return parentId;
+    }
+
+    public Builder setParentId(Integer parentId) {
+      this.parentId = parentId;
       return this;
     }
   }
