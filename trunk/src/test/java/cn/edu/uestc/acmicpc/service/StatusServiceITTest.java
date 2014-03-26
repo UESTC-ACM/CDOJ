31ca752e13ad151743f2fbae352cd480415042dc
Jianjin Fan <fanjianjin@gmail.com>
2014-03-26 23:37:56 +0800

    add test cases of count.

--- trunk/src/test/java/cn/edu/uestc/acmicpc/service/StatusServiceITTest.java
+++ trunk/src/test/java/cn/edu/uestc/acmicpc/service/StatusServiceITTest.java
@@ -1,8 +1,10 @@
 package cn.edu.uestc.acmicpc.service;
 
 import cn.edu.uestc.acmicpc.config.IntegrationTestContext;
+import cn.edu.uestc.acmicpc.db.condition.impl.StatusCondition;
 import cn.edu.uestc.acmicpc.service.iface.StatusService;
 import cn.edu.uestc.acmicpc.util.exception.AppException;
+import cn.edu.uestc.acmicpc.util.settings.Global;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -10,7 +12,9 @@ import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.sql.Timestamp;
 import java.util.Arrays;
+import java.util.HashSet;
 
 /**
  * test cases for {@link cn.edu.uestc.acmicpc.service.iface.StatusService}
@@ -118,4 +122,206 @@ public class StatusServiceITTest extends AbstractTestNGSpringContextTests {
     Integer problemId2 = 2;
     Assert.assertEquals(statusService.countUsersAcceptedProblem(problemId2), Long.valueOf(1));
   }
+
+  @Test
+  public void testCount_emptyCondition_isNotAdmin() throws AppException {
+    StatusCondition statusCondition = new StatusCondition();
+    Assert.assertEquals(statusService.count(statusCondition), Long.valueOf(2L));
+  }
+
+  @Test
+  public void testCount_emptyCondition_isAdmin() throws AppException {
+    StatusCondition statusCondition = new StatusCondition();
+    statusCondition.isForAdmin = true;
+    Assert.assertEquals(statusService.count(statusCondition), Long.valueOf(7L));
+  }
+
+  @Test
+  public void testCount_byStartId() throws AppException {
+    StatusCondition statusCondition = new StatusCondition();
+    statusCondition.isForAdmin = true;
+    statusCondition.startId = 2;
+    Assert.assertEquals(statusService.count(statusCondition), Long.valueOf(6L));
+  }
+
+  @Test
+  public void testCount_byEndId() throws AppException {
+    StatusCondition statusCondition = new StatusCondition();
+    statusCondition.isForAdmin = true;
+    statusCondition.endId = 5;
+    Assert.assertEquals(statusService.count(statusCondition), Long.valueOf(5L));
+  }
+
+  @Test
+  public void testCount_byStartIdAndEndId() throws AppException {
+    StatusCondition statusCondition = new StatusCondition();
+    statusCondition.isForAdmin = true;
+    statusCondition.startId = 3;
+    statusCondition.endId = 4;
+    Assert.assertEquals(statusService.count(statusCondition), Long.valueOf(2L));
+  }
+
+  @Test
+  public void testCount_byStartIdAndEndId_emptyResult() throws AppException {
+    StatusCondition statusCondition = new StatusCondition();
+    statusCondition.isForAdmin = true;
+    statusCondition.startId = 4;
+    statusCondition.endId = 3;
+    Assert.assertEquals(statusService.count(statusCondition), Long.valueOf(0L));
+  }
+
+  @Test
+  public void testCount_byUserId() throws AppException {
+    StatusCondition statusCondition = new StatusCondition();
+    statusCondition.isForAdmin = true;
+    statusCondition.userId = 3;
+    Assert.assertEquals(statusService.count(statusCondition), Long.valueOf(2L));
+  }
+
+  @Test
+  public void testCount_byUserId_emptyResult() throws AppException {
+    StatusCondition statusCondition = new StatusCondition();
+    statusCondition.isForAdmin = true;
+    statusCondition.userId = 4;
+    Assert.assertEquals(statusService.count(statusCondition), Long.valueOf(0L));
+  }
+
+  @Test
+  public void testCount_byProblemId() throws AppException {
+    StatusCondition statusCondition = new StatusCondition();
+    statusCondition.isForAdmin = true;
+    statusCondition.problemId = 1;
+    Assert.assertEquals(statusService.count(statusCondition), Long.valueOf(6L));
+  }
+
+  @Test
+  public void testCount_byProblemId_emptyResult() throws AppException {
+    StatusCondition statusCondition = new StatusCondition();
+    statusCondition.isForAdmin = true;
+    statusCondition.problemId = 3;
+    Assert.assertEquals(statusService.count(statusCondition), Long.valueOf(0L));
+  }
+
+  @Test
+  public void testCount_byResult() throws AppException {
+    StatusCondition statusCondition = new StatusCondition();
+    statusCondition.isForAdmin = true;
+    statusCondition.result = Global.OnlineJudgeResultType.OJ_AC;
+    Assert.assertEquals(statusService.count(statusCondition), Long.valueOf(2L));
+  }
+
+  @Test
+  public void testCount_byResult_emptyResult() throws AppException {
+    StatusCondition statusCondition = new StatusCondition();
+    statusCondition.isForAdmin = true;
+    statusCondition.result = Global.OnlineJudgeResultType.OJ_SE;
+    Assert.assertEquals(statusService.count(statusCondition), Long.valueOf(0L));
+  }
+
+  @Test
+  public void testCount_byResults() throws AppException {
+    StatusCondition statusCondition = new StatusCondition();
+    statusCondition.isForAdmin = true;
+    statusCondition.results = new HashSet<>(Arrays.asList(
+        Global.OnlineJudgeResultType.OJ_MLE,
+        Global.OnlineJudgeResultType.OJ_CE,
+        Global.OnlineJudgeResultType.OJ_OLE
+    ));
+    Assert.assertEquals(statusService.count(statusCondition), Long.valueOf(2L));
+  }
+
+  @Test
+  public void testCount_byResults_emptyResult() throws AppException {
+    StatusCondition statusCondition = new StatusCondition();
+    statusCondition.isForAdmin = true;
+    statusCondition.results = new HashSet<>(Arrays.asList(
+        Global.OnlineJudgeResultType.OJ_JUDGING,
+        Global.OnlineJudgeResultType.OJ_RE,
+        Global.OnlineJudgeResultType.OJ_RF
+    ));
+    Assert.assertEquals(statusService.count(statusCondition), Long.valueOf(0L));
+  }
+
+  @Test
+  public void testCount_byLanguageId() throws AppException {
+    StatusCondition statusCondition = new StatusCondition();
+    statusCondition.isForAdmin = true;
+    statusCondition.languageId = 1;
+    Assert.assertEquals(statusService.count(statusCondition), Long.valueOf(7L));
+  }
+
+  @Test
+  public void testCount_byLanguageId_emptyResult() throws AppException {
+    StatusCondition statusCondition = new StatusCondition();
+    statusCondition.isForAdmin = true;
+    statusCondition.languageId = 2;
+    Assert.assertEquals(statusService.count(statusCondition), Long.valueOf(0L));
+  }
+
+  @Test
+  public void testCount_byUserName() throws AppException {
+    StatusCondition statusCondition = new StatusCondition();
+    statusCondition.isForAdmin = true;
+    statusCondition.userName = "administrator";
+    Assert.assertEquals(statusService.count(statusCondition), Long.valueOf(3L));
+  }
+
+  @Test
+  public void testCount_byUserName_emptyResult_noStatus() throws AppException {
+    StatusCondition statusCondition = new StatusCondition();
+    statusCondition.isForAdmin = true;
+    statusCondition.userName = "user5";
+    Assert.assertEquals(statusService.count(statusCondition), Long.valueOf(0L));
+  }
+
+  @Test
+  public void testCount_byUserName_emptyResult_noUser() throws AppException {
+    StatusCondition statusCondition = new StatusCondition();
+    statusCondition.isForAdmin = true;
+    statusCondition.userName = "non_existed_user";
+    Assert.assertEquals(statusService.count(statusCondition), Long.valueOf(0L));
+  }
+
+  @Test
+  public void testCount_byStartTime() throws AppException {
+    StatusCondition statusCondition = new StatusCondition();
+    statusCondition.isForAdmin = true;
+    statusCondition.startTime = Timestamp.valueOf("2014-01-01 00:00:00");
+    Assert.assertEquals(statusService.count(statusCondition), Long.valueOf(2L));
+  }
+
+  @Test
+  public void testCount_byEndTime() throws AppException {
+    StatusCondition statusCondition = new StatusCondition();
+    statusCondition.isForAdmin = true;
+    statusCondition.endTime = Timestamp.valueOf("2014-03-26 10:59:59");
+    Assert.assertEquals(statusService.count(statusCondition), Long.valueOf(6L));
+  }
+
+  @Test
+  public void testCount_byStartTimeAndEndTime() throws AppException {
+    StatusCondition statusCondition = new StatusCondition();
+    statusCondition.isForAdmin = true;
+    statusCondition.endTime = Timestamp.valueOf("2014-03-01 00:00:00");
+    statusCondition.startTime = Timestamp.valueOf("2013-11-01 00:00:00");
+    Assert.assertEquals(statusService.count(statusCondition), Long.valueOf(1L));
+  }
+
+  @Test
+  public void testCount_byStartTimeAndEndTime_emptyResult() throws AppException {
+    StatusCondition statusCondition = new StatusCondition();
+    statusCondition.isForAdmin = true;
+    statusCondition.endTime = Timestamp.valueOf("2013-01-01 00:00:00");
+    statusCondition.startTime = Timestamp.valueOf("2014-01-01 00:00:00");
+    Assert.assertEquals(statusService.count(statusCondition), Long.valueOf(0L));
+  }
+
+  @Test
+  public void testCount_byContestId() throws AppException {
+    StatusCondition statusCondition = new StatusCondition();
+    statusCondition.isForAdmin = true;
+    statusCondition.contestId = 1;
+    Assert.assertEquals(statusService.count(statusCondition), Long.valueOf(2L));
+  }
+
 }
