703215c78082272d45028caf11e5f8b156f2210e
mzry1992 <muziriyun@gmail.com>
2014-03-31 20:28:32 +0800

    Add settings db tools

---
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/db/dao/iface/ISettingsDAO.java
@@ -0,0 +1,9 @@
+package cn.edu.uestc.acmicpc.db.dao.iface;
+
+import cn.edu.uestc.acmicpc.db.entity.Settings;
+
+/**
+ * SettingsDAO AOP interface.
+ */
+public interface ISettingsDAO extends IDAO<Settings, Integer> {
+}
