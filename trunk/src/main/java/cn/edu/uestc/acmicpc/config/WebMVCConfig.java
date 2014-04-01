417d12655ca8ff52a63c285fdcd0373e49d8c0a9
mzry1992 <muziriyun@gmail.com>
2014-04-01 20:26:09 +0800

    Set static resources path

--- trunk/src/main/java/cn/edu/uestc/acmicpc/config/WebMVCConfig.java
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/config/WebMVCConfig.java
@@ -3,10 +3,13 @@ package cn.edu.uestc.acmicpc.config;
 import cn.edu.uestc.acmicpc.web.view.ContestRankListView;
 import cn.edu.uestc.acmicpc.web.view.ContestRegistryReportView;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
@@ -28,16 +31,25 @@ import java.util.List;
 @ComponentScan(basePackages = {
     "cn.edu.uestc.acmicpc.web.oj.controller"
 })
+@PropertySource("classpath:resources.properties")
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class WebMVCConfig extends WebMvcConfigurerAdapter {
 
+  @Autowired
+  private Environment environment;
+
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("/images/**").addResourceLocations("/images/**");
-    registry.addResourceHandler("/plugins/**").addResourceLocations("/plugins/**");
-    registry.addResourceHandler("/scripts/**").addResourceLocations("/scripts/**");
-    registry.addResourceHandler("/styles/**").addResourceLocations("/styles/**");
-    registry.addResourceHandler("/font/**").addResourceLocations("/font/**");
+    registry.addResourceHandler("/cdoj/**").addResourceLocations(getResourceLocations("cdoj"));
+    registry.addResourceHandler("/font/**").addResourceLocations(getResourceLocations("font"));
+    registry.addResourceHandler("/images/**").addResourceLocations(getResourceLocations("images"));
+    registry.addResourceHandler("/plugins/**").addResourceLocations(getResourceLocations("plugins"));
+    registry.addResourceHandler("/template/**").addResourceLocations(getResourceLocations("template"));
+  }
+
+  private String getResourceLocations(String folder) {
+    String path = environment.getProperty("staticResources.path");
+    return "file://" + path + "/" + folder + "/**";
   }
 
   @Override
