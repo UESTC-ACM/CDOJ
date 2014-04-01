c8537b0a431940bef8e125eb9a94cd2fc4ba8b33
mzry1992 <muziriyun@gmail.com>
2014-04-01 20:26:09 +0800

    Set static resources path

--- trunk/scripts/configure_project.sh
+++ trunk/scripts/configure_project.sh
@@ -3,16 +3,18 @@
 # configure project
 #
 # parameters:
-# total: 4
+# total: 5
 # $1 original file
 # $2 destination file
 # $3 user for db
 # $4 password for db
+# $5 static resources path
 
-if [ $# -ne 4 ]; then
+if [ $# -ne 5 ]; then
   exit 1
 fi
 
 cp $1 $2
 echo "db.username=$3" >> $2
 echo "db.password=$4" >> $2
+echo "staticResources.path=$5" >> $2
