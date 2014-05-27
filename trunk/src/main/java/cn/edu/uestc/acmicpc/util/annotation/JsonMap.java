edf8ad2d7ce2df51329d5cff94755315bbca8db7
mzry1992 <muziriyun@gmail.com>
2014-05-27 20:15:11 +0800

    Add Custom json argument resolver

--- trunk/src/main/java/cn/edu/uestc/acmicpc/util/annotation/JsonMap.java
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/util/annotation/JsonMap.java
@@ -1,9 +1,17 @@
 package cn.edu.uestc.acmicpc.util.annotation;
 
-/**
- * Description
- */
-public @interface JsonMap {
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+/**
+ * Annotation for mark the argument need resolved from json data.
+ */
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface JsonMap {
   public String value() default "";
 }
