b97b67148ee7ee7751b0e42ac840ba12e2d2b576
mzry1992 <muziriyun@gmail.com>
2014-06-24 16:51:49 +0800

    Add training database tools

---
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/db/criteria/impl/TrainingCriteria.java
@@ -0,0 +1,50 @@
+package cn.edu.uestc.acmicpc.db.criteria.impl;
+
+import cn.edu.uestc.acmicpc.db.criteria.base.BaseCriteria;
+import cn.edu.uestc.acmicpc.db.dto.field.Fields;
+import cn.edu.uestc.acmicpc.db.dto.impl.TrainingDto;
+import cn.edu.uestc.acmicpc.db.entity.Training;
+import cn.edu.uestc.acmicpc.util.exception.AppException;
+
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Restrictions;
+
+/**
+ * Training database criteria entity.
+ */
+public class TrainingCriteria extends BaseCriteria<Training, TrainingDto> {
+
+  public TrainingCriteria(Fields resultFields) {
+    super(Training.class, TrainingDto.class, resultFields);
+  }
+
+  public TrainingCriteria() {
+    super(Training.class, TrainingDto.class);
+  }
+
+  @Override
+  public void setResultFields(Fields resultFields) {
+    super.setResultFields(resultFields);
+  }
+
+  public Integer startId;
+  public Integer endId;
+  public String keyword;
+
+  @Override
+  public DetachedCriteria getCriteria() throws AppException {
+    DetachedCriteria criteria = super.getCriteria();
+
+    if (startId != null) {
+      criteria.add(Restrictions.ge("trainingId", startId));
+    }
+    if (endId != null) {
+      criteria.add(Restrictions.le("trainingId", endId));
+    }
+    if (keyword != null) {
+      criteria.add(Restrictions.ilike("title", keyword));
+    }
+
+    return criteria;
+  }
+}
---
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/service/iface/TrainingService.java
@@ -0,0 +1,65 @@
+package cn.edu.uestc.acmicpc.service.iface;
+
+import cn.edu.uestc.acmicpc.db.criteria.impl.TrainingCriteria;
+import cn.edu.uestc.acmicpc.db.dto.field.TrainingFields;
+import cn.edu.uestc.acmicpc.db.dto.impl.TrainingDto;
+import cn.edu.uestc.acmicpc.db.entity.Training;
+import cn.edu.uestc.acmicpc.util.exception.AppException;
+import cn.edu.uestc.acmicpc.web.dto.PageInfo;
+
+import java.util.List;
+
+/**
+ * Training service interface.
+ */
+public interface TrainingService extends DatabaseService<Training, Integer> {
+
+  /**
+   * Get {@link TrainingDto} by Training id.
+   *
+   * @param trainingId training record's id.
+   * @param trainingFields request fields.
+   * @return result {@link TrainingDto} entity.
+   * @throws AppException
+   */
+  public TrainingDto getTrainingDto(Integer trainingId,
+                                    TrainingFields trainingFields) throws AppException;
+
+  /**
+   * Count number of trainings fit in criteria.
+   *
+   * @param trainingCriteria search criteria
+   * @return total records in database hit the criteria
+   * @throws AppException
+   */
+  public Long count(TrainingCriteria trainingCriteria) throws AppException;
+
+  /**
+   * Get all {@link TrainingDto} fit in criteria.
+   *
+   * @param trainingCriteria search criteria
+   * @param pageInfo page range restriction
+   * @return all records in database hit the criteria
+   * @throws AppException
+   */
+  public List<TrainingDto> getTrainingList(TrainingCriteria trainingCriteria,
+                                           PageInfo pageInfo) throws AppException;
+
+  /**
+   * Update training record by none-null fields in {@link TrainingDto} entity
+   *
+   * @param trainingDto {@link TrainingDto} entity with none-null id
+   * @throws AppException
+   */
+  public void updateTraining(TrainingDto trainingDto) throws AppException;
+
+  /**
+   * Create a new training record with specified title
+   *
+   * @param title training title
+   * @return Id of new record
+   * @throws AppException
+   */
+  public Integer createNewTraining(String title) throws AppException;
+
+}
