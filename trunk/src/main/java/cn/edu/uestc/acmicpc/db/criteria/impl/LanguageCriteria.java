1010f4a19d552770a93ed43d64830cdb0bce54e6
Ruins He <lyhypacm@gmail.com>
2015-02-15 22:55:11 +0800

    refactoring - move field and base dto location.

--- trunk/src/main/java/cn/edu/uestc/acmicpc/db/criteria/impl/LanguageCriteria.java
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/db/criteria/impl/LanguageCriteria.java
@@ -1,7 +1,7 @@
 package cn.edu.uestc.acmicpc.db.criteria.impl;
 
 import cn.edu.uestc.acmicpc.db.criteria.base.BaseCriteria;
-import cn.edu.uestc.acmicpc.db.dto.field.Fields;
+import cn.edu.uestc.acmicpc.db.dto.Fields;
 import cn.edu.uestc.acmicpc.db.dto.impl.LanguageDto;
 import cn.edu.uestc.acmicpc.db.entity.Language;
 
