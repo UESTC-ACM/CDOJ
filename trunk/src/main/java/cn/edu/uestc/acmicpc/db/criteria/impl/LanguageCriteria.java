0ce9f9f2078dfe7c37a8caa555de93696358ceec
Ruins He <lyhypacm@gmail.com>
2015-02-15 22:51:34 +0800

    simplify DepartmentDto.

--- trunk/src/main/java/cn/edu/uestc/acmicpc/db/criteria/impl/LanguageCriteria.java
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/db/criteria/impl/LanguageCriteria.java
@@ -4,9 +4,6 @@ import cn.edu.uestc.acmicpc.db.criteria.base.BaseCriteria;
 import cn.edu.uestc.acmicpc.db.dto.field.Fields;
 import cn.edu.uestc.acmicpc.db.dto.impl.LanguageDto;
 import cn.edu.uestc.acmicpc.db.entity.Language;
-import cn.edu.uestc.acmicpc.util.exception.AppException;
-
-import org.hibernate.criterion.DetachedCriteria;
 
 /**
  * Article database criteria entity.
@@ -19,11 +16,5 @@ public class LanguageCriteria extends BaseCriteria<Language, LanguageDto> {
 
   public LanguageCriteria() {
     this(null);
-  }
-
-  @Override
-  public DetachedCriteria getCriteria() throws AppException {
-    DetachedCriteria criteria = super.getCriteria();
-    return criteria;
   }
 }
