992daa26c46a05b07412eb4426e94611a0e7a1f2
Yun Li <muziriyun@gmail.com>
2015-03-04 00:32:16 +0800

    Refactor judge service and add fake judge

--- trunk/src/main/java/cn/edu/uestc/acmicpc/judge/entity/Judge.java
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/judge/entity/Judge.java
@@ -1,22 +1,15 @@
 package cn.edu.uestc.acmicpc.judge.entity;
 
+import cn.edu.uestc.acmicpc.judge.core.JudgeCore;
+import cn.edu.uestc.acmicpc.judge.core.JudgeResult;
 import cn.edu.uestc.acmicpc.util.enums.OnlineJudgeReturnType;
-import cn.edu.uestc.acmicpc.util.exception.AppException;
-import cn.edu.uestc.acmicpc.util.helper.FileUtil;
-import cn.edu.uestc.acmicpc.util.settings.Settings;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.concurrent.BlockingQueue;
 
 /**
@@ -32,41 +25,10 @@ public class Judge implements Runnable {
     this.judgeName = judgeName;
   }
 
-  public void setWorkPath(String workPath) throws AppException {
-    File dir = new File(workPath);
-    if (!dir.exists() && !dir.mkdirs()) {
-      throw new AppException("Cannot create work directory");
-    }
-    this.workPath = workPath;
-  }
-
   /**
    * Judge's name.
    */
   private String judgeName;
-  /**
-   * Judge's work path.
-   */
-  private String workPath;
-
-  /**
-   * Global setting entity.
-   */
-  @Autowired
-  private Settings settings;
-
-  public void setTempPath(String tempPath) throws AppException {
-    File dir = new File(tempPath);
-    if (!dir.exists() && !dir.mkdirs()) {
-      throw new AppException("Cannot create temp directory");
-    }
-    this.tempPath = tempPath;
-  }
-
-  /**
-   * Temp files path.
-   */
-  private String tempPath;
 
   public void setJudgeQueue(BlockingQueue<JudgeItem> judgeQueue) {
     this.judgeQueue = judgeQueue;
@@ -76,6 +38,15 @@ public class Judge implements Runnable {
    * Global judge queue.
    */
   private BlockingQueue<JudgeItem> judgeQueue;
+
+  /**
+   * Judge core
+   */
+  private JudgeCore judgeCore;
+
+  public void setJudgeCore(JudgeCore judgeCore) {
+    this.judgeCore = judgeCore;
+  }
 
   @Override
   public void run() {
@@ -93,99 +64,6 @@ public class Judge implements Runnable {
   }
 
   /**
-   * Build judge's core shell command line
-   *
-   * @param problemId
-   *          problem's id
-   * @param currentTestCase
-   *          current test case number
-   * @param judgeItem
-   *          {@code judgeItem} entity
-   * @return command line we need
-   */
-  private String buildJudgeShellCommand(int problemId, int currentTestCase,
-      JudgeItem judgeItem) {
-    StringBuilder stringBuilder = new StringBuilder();
-
-    stringBuilder.append(settings.JUDGE_CORE);
-    stringBuilder.append(" -T "); // SPJ time
-    stringBuilder.append(10000);
-    stringBuilder.append(" -L "); // log file
-    stringBuilder.append(workPath + "/log.log");
-
-    stringBuilder.append(" -u ");
-    stringBuilder.append(judgeItem.getStatus().getStatusId());
-    stringBuilder.append(" -s ");
-    stringBuilder.append(judgeItem.getSourceName());
-    stringBuilder.append(" -n ");
-    stringBuilder.append(problemId);
-    stringBuilder.append(" -D ");
-    stringBuilder.append(settings.DATA_PATH);
-    stringBuilder.append("/")
-        .append(judgeItem.getStatus().getProblemId())
-        .append("/");
-    stringBuilder.append(" -d ");
-    stringBuilder.append(tempPath);
-
-    if (judgeItem.getStatus().getLanguage().equals("Java")) {
-      stringBuilder.append(" -t ");
-      stringBuilder.append(judgeItem.getStatus().getJavaTimeLimit());
-      stringBuilder.append(" -m ");
-      stringBuilder.append(judgeItem.getStatus()
-          .getJavaMemoryLimit());
-    } else {
-      stringBuilder.append(" -t ");
-      stringBuilder.append(judgeItem.getStatus().getTimeLimit());
-      stringBuilder.append(" -m ");
-      stringBuilder.append(judgeItem.getStatus().getMemoryLimit());
-    }
-
-    stringBuilder.append(" -o ");
-    stringBuilder.append(judgeItem.getStatus().getOutputLimit());
-    if (judgeItem.getStatus().getIsSpj()) {
-      stringBuilder.append(" -S");
-    }
-    stringBuilder.append(" -l ");
-    stringBuilder.append(judgeItem.getStatus().getLanguageId());
-    stringBuilder.append(" -I ");
-    stringBuilder.append(settings.DATA_PATH).append("/")
-        .append(judgeItem.getStatus().getProblemId()).append("/")
-        .append(currentTestCase).append(".in");
-    stringBuilder.append(" -O ");
-    stringBuilder.append(settings.DATA_PATH).append("/")
-        .append(judgeItem.getStatus().getProblemId()).append("/")
-        .append(currentTestCase).append(".out");
-    if (currentTestCase == 1) {
-      stringBuilder.append(" -C");
-    }
-
-    return stringBuilder.toString();
-  }
-
-  /**
-   * Get process' call back string with shell command.
-   *
-   * @param shellCommand
-   *          shell command line
-   * @return command's call back string
-   */
-  private String[] getCallBackString(String shellCommand) {
-    Process p;
-    String callBackString = "";
-    try {
-      p = Runtime.getRuntime().exec(shellCommand);
-      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
-          p.getInputStream()));
-      String line;
-      while ((line = bufferedReader.readLine()) != null) {
-        callBackString += line;
-      }
-    } catch (Exception ignored) {
-    }
-    return callBackString.split(" ");
-  }
-
-  /**
    * Judge judgeItem by judge core.
    *
    * @param judgeItem
@@ -197,16 +75,10 @@ public class Judge implements Runnable {
     try {
       int numberOfTestCase = judgeItem.getStatus().getDataCount();
       boolean isAccepted = true;
-      FileUtil.saveToFile(judgeItem.getStatus().getCodeContent(),
-          tempPath + "/"
-              + judgeItem.getSourceName());
-      int problemId = judgeItem.getStatus().getProblemId();
       for (int currentCase = 1; isAccepted && currentCase <= numberOfTestCase; currentCase++) {
         judgeItem.getStatus().setCaseNumber(currentCase);
-        String shellCommand = buildJudgeShellCommand(problemId, currentCase,
-            judgeItem);
-        String[] callBackString = getCallBackString(shellCommand);
-        isAccepted = updateJudgeItem(callBackString, judgeItem);
+        JudgeResult result = judgeCore.judge(currentCase, judgeItem);
+        isAccepted = updateJudgeItem(result, judgeItem);
       }
       if (isAccepted) {
         judgeItem.getStatus().setResultId(OnlineJudgeReturnType.OJ_AC.ordinal());
@@ -219,61 +91,29 @@ public class Judge implements Runnable {
     }
   }
 
-  private boolean updateJudgeItem(String[] callBackString, JudgeItem judgeItem) {
+  private boolean updateJudgeItem(JudgeResult judgeResult, JudgeItem judgeItem) {
     boolean isAccepted = true;
-    if (callBackString != null && callBackString.length == 3) {
-      try {
-        int result = Integer.parseInt(callBackString[0]);
-        if (result == OnlineJudgeReturnType.OJ_AC.ordinal()) {
-          result = OnlineJudgeReturnType.OJ_RUNNING.ordinal();
-        } else {
-          isAccepted = false;
-        }
-        judgeItem.getStatus().setResultId(result);
-        Integer oldMemoryCost = judgeItem.getStatus().getMemoryCost();
-        Integer currentMemoryCost = Integer.parseInt(callBackString[1]);
-        judgeItem.getStatus().setMemoryCost(Math.max(currentMemoryCost, oldMemoryCost));
-
-        Integer oldTimeCost = judgeItem.getStatus().getTimeCost();
-        Integer currentTimeCost = Integer.parseInt(callBackString[2]);
-        if (oldTimeCost == null) {
-          judgeItem.getStatus().setTimeCost(currentTimeCost);
-        } else {
-          judgeItem.getStatus().setTimeCost(Math.max(currentTimeCost, oldTimeCost));
-        }
-      } catch (NumberFormatException e) {
-        judgeItem.getStatus().setResultId(OnlineJudgeReturnType.OJ_SE.ordinal());
-        isAccepted = false;
-      }
+    OnlineJudgeReturnType result = judgeResult.getResult();
+    if (result == OnlineJudgeReturnType.OJ_AC) {
+      result = OnlineJudgeReturnType.OJ_RUNNING;
     } else {
-      judgeItem.getStatus().setResultId(OnlineJudgeReturnType.OJ_SE.ordinal());
       isAccepted = false;
+    }
+    judgeItem.getStatus().setResultId(result.ordinal());
+    Integer oldMemoryCost = judgeItem.getStatus().getMemoryCost();
+    Integer currentMemoryCost = judgeResult.getMemoryCost();
+    judgeItem.getStatus().setMemoryCost(Math.max(currentMemoryCost, oldMemoryCost));
+
+    Integer oldTimeCost = judgeItem.getStatus().getTimeCost();
+    Integer currentTimeCost = judgeResult.getTimeCost();
+    if (oldTimeCost == null) {
+      judgeItem.getStatus().setTimeCost(currentTimeCost);
+    } else {
+      judgeItem.getStatus().setTimeCost(Math.max(currentTimeCost, oldTimeCost));
     }
 
     if (judgeItem.getStatus().getResultId() == OnlineJudgeReturnType.OJ_CE.ordinal()) {
-      StringBuilder stringBuilder = new StringBuilder();
-      BufferedReader br = null;
-      try {
-        br = new BufferedReader(new FileReader(workPath
-            + "/temp/stderr_compiler.txt"));
-        String line;
-        while ((line = br.readLine()) != null) {
-          if (line.trim().startsWith("/home/")) {
-            line = line.substring(line.indexOf(judgeItem.getSourceName()));
-          }
-          stringBuilder.append(line).append('\n');
-        }
-      } catch (Exception e) {
-        e.printStackTrace();
-      } finally {
-        if (br != null) {
-          try {
-            br.close();
-          } catch (IOException ignored) {
-          }
-        }
-      }
-      judgeItem.setCompileInfo(stringBuilder.toString());
+      judgeItem.setCompileInfo(judgeResult.getCompileInfo());
     } else {
       if (judgeItem.getCompileInfo() != null) {
         judgeItem.setCompileInfo(null);
