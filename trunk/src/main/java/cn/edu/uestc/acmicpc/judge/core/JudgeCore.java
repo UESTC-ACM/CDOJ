aad43d35ce83dc92f32f20b75a8dc6100df8628a
Yun Li <muziriyun@gmail.com>
2015-03-04 14:16:06 +0800

    Remove unused deps

--- trunk/src/main/java/cn/edu/uestc/acmicpc/judge/core/JudgeCore.java
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/judge/core/JudgeCore.java
@@ -1,7 +1,6 @@
 package cn.edu.uestc.acmicpc.judge.core;
 
 import cn.edu.uestc.acmicpc.judge.entity.JudgeItem;
-import cn.edu.uestc.acmicpc.util.enums.OnlineJudgeReturnType;
 
 /**
  * Judge core interface
