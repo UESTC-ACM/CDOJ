045124a92a300ca393ac2d687e168509487b78f1
Yun Li <muziriyun@gmail.com>
2015-03-09 00:25:22 +0800

    Remove deprecated tests, fix AOPITTest and CriteriaITTest

--- trunk/src/test/java/cn/edu/uestc/acmicpc/db/criteria/CriteriaITTest.java
+++ trunk/src/test/java/cn/edu/uestc/acmicpc/db/criteria/CriteriaITTest.java
@@ -3,12 +3,15 @@ package cn.edu.uestc.acmicpc.db.criteria;
 import cn.edu.uestc.acmicpc.db.dao.iface.ArticleDao;
 import cn.edu.uestc.acmicpc.db.dto.field.ArticleFields;
 import cn.edu.uestc.acmicpc.db.dto.impl.ArticleDto;
+import cn.edu.uestc.acmicpc.db.entity.Article;
 import cn.edu.uestc.acmicpc.testing.PersistenceITTest;
 import cn.edu.uestc.acmicpc.util.exception.AppException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static com.google.common.truth.Truth.assertThat;
 
 import com.google.common.collect.ImmutableSet;
 
@@ -21,46 +24,35 @@ public class CriteriaITTest extends PersistenceITTest {
 
   @Autowired
   ArticleDao articleDao;
-
+  
+  @Test
   public void testFetchArticle_withSpecifiedFields() throws AppException {
+    ArticleDto article = articleProvider.createArticle(testUserId);
+
     ArticleCriteria articleCriteria = new ArticleCriteria();
 
-    articleCriteria.startId = 1;
-    articleCriteria.endId = 1;
+    articleCriteria.startId = article.getArticleId();
+    articleCriteria.endId = article.getArticleId();
 
     List<ArticleDto> result = articleDao.findAll(articleCriteria, null,
         ImmutableSet.of(ArticleFields.ARTICLE_ID));
-    Assert.assertEquals(result.size(), 1);
+
+    assertThat(result).hasSize(1);
     ArticleDto articleDto = result.get(0);
 
-    Assert.assertEquals(articleDto.getArticleId(), Integer.valueOf(1));
-    Assert.assertNull(articleDto.getTitle());
-    Assert.assertNull(articleDto.getContent());
-    Assert.assertNull(articleDto.getTime());
-    Assert.assertEquals(articleDto.getClicked(), Integer.valueOf(0));
-    Assert.assertEquals(articleDto.getOrder(), Integer.valueOf(0));
-    Assert.assertEquals(articleDto.getType(), Integer.valueOf(0));
-    Assert.assertEquals(articleDto.getIsVisible(), Boolean.FALSE);
-    Assert.assertNull(articleDto.getParentId());
-    Assert.assertNull(articleDto.getProblemId());
-    Assert.assertNull(articleDto.getContestId());
-    Assert.assertEquals(articleDto.getUserId(), Integer.valueOf(1));
-    Assert.assertNull(articleDto.getOwnerName());
-    Assert.assertNull(articleDto.getOwnerEmail());
-  }
-
-  @Test
-  public void test_fetch_article_list_by_id_range_successful() throws AppException {
-    ArticleCriteria articleCriteria = new ArticleCriteria();
-
-    articleCriteria.startId = 2;
-    articleCriteria.endId = 3;
-
-    List<ArticleDto> result = articleDao.findAll(articleCriteria, null,
-        ArticleFields.ALL_FIELDS);
-
-    Assert.assertEquals(result.size(), 2);
-    Assert.assertEquals(result.get(0).getArticleId(), Integer.valueOf(2));
-    Assert.assertEquals(result.get(1).getArticleId(), Integer.valueOf(3));
+    assertThat(articleDto.getArticleId()).isEqualTo(article.getArticleId());
+    assertThat(articleDto.getTitle()).isNull();
+    assertThat(articleDto.getContent()).isNull();
+    assertThat(articleDto.getTime()).isNull();
+    assertThat(articleDto.getClicked()).isNull();
+    assertThat(articleDto.getOrder()).isNull();
+    assertThat(articleDto.getType()).isNull();
+    assertThat(articleDto.getIsVisible()).isNull();
+    assertThat(articleDto.getParentId()).isNull();
+    assertThat(articleDto.getProblemId()).isNull();
+    assertThat(articleDto.getContestId()).isNull();
+    assertThat(articleDto.getUserId()).isNull();
+    assertThat(articleDto.getOwnerName()).isNull();
+    assertThat(articleDto.getOwnerEmail()).isNull();
   }
 }
