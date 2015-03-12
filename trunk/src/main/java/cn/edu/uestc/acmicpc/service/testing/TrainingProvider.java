02dd064345ccde282b7d3b713dc57c7c5656d7f1
Ruins He <lyhypacm@gmail.com>
2015-03-12 21:28:33 +0800

    add training related providers.

---
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/service/testing/TrainingProvider.java
@@ -0,0 +1,24 @@
+package cn.edu.uestc.acmicpc.service.testing;
+
+import cn.edu.uestc.acmicpc.db.dto.field.TrainingFields;
+import cn.edu.uestc.acmicpc.db.dto.impl.TrainingDto;
+import cn.edu.uestc.acmicpc.service.iface.TrainingService;
+import cn.edu.uestc.acmicpc.util.exception.AppException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Training provider for integration testing.
+ */
+@Component
+public class TrainingProvider {
+
+  @Autowired
+  private TrainingService service;
+
+  public TrainingDto createTraining() throws AppException {
+    Integer id = service.createNewTraining("training " + TestUtil.getUniqueId());
+    return service.getTrainingDto(id, TrainingFields.ALL_FIELDS);
+  }
+}
