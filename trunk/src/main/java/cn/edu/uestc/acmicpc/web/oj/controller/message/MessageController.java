dd937e72f14fe3a5fd72426665836eb1e1afcf25
LaPluses <xipersup@gmail.com>
2018-03-18 20:42:40 +0800

    Fix compile error

--- trunk/src/main/java/cn/edu/uestc/acmicpc/web/oj/controller/message/MessageController.java
+++ trunk/src/main/java/cn/edu/uestc/acmicpc/web/oj/controller/message/MessageController.java
@@ -18,7 +18,6 @@ import java.util.Objects;
 import javax.servlet.http.HttpSession;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-import org.omg.CORBA.Current;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
